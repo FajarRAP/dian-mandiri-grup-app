b67c01c686dab8e2305f0bf15979beaa8533fa8c
FajarRiansyah <fajary781@gmail.com>
2024-12-25 10:47:45 +0700

    refactor (auth repository): migrated from supabase complete

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/failure/failure.dart';
+import '../entities/user_entity.dart';
+
+abstract class AuthRepository {
+  Future<Either<Failure, UserEntity>> signIn();
+  Future<Either<Failure, String>> signOut();
+  Future<Either<Failure, UserEntity>> fetchUser();
+  Future<Either<Failure, String>> refreshToken({required String refreshToken});
+  Future<Either<Failure, UserEntity>> fetchUserFromStorage();
+  Future<Either<Failure, String>> updateProfile({required String name});
+}
