8305ec34455fef1d03aeb9bb011454e498648027
FajarRiansyah <fajary781@gmail.com>
2024-12-25 12:10:51 +0700

    refactor (shipment repository contract): migrated from supabase complete

---
+++ lib/features/tracker/domain/repositories/shipment_repository.dart
@@ -0,0 +1,34 @@
+import 'package:camera/camera.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/failure/failure.dart';
+import '../entities/shipment_detail_entity.dart';
+import '../entities/shipment_entity.dart';
+import '../entities/shipment_report_entity.dart';
+
+abstract class ShipmentRepository {
+  Future<Either<Failure, List<ShipmentEntity>>> fetchShipments(
+      {required String date,
+      required String stage,
+      int page = 1,
+      String? keyword});
+  Future<Either<Failure, String>> insertShipment(
+      {required String receiptNumber, required String stage});
+  Future<Either<Failure, List<ShipmentReportEntity>>> fetchShipmentReports(
+      {required String startDate,
+      required String endDate,
+      required String status});
+  Future<Either<Failure, String>> createShipmentReport(
+      {required String startDate, required String endDate});
+  Future<Either<Failure, ShipmentDetailEntity>> fetchShipmentByReceiptNumber(
+      {required String receiptNumber});
+  Future<Either<Failure, ShipmentDetailEntity>> fetchShipmentById(
+      {required String shipmentId});
+  Future<Either<Failure, String>> deleteShipment({required String shipmentId});
+  Future<Either<Failure, String>> insertShipmentDocument(
+      {required String shipmentId,
+      required XFile document,
+      required String stage});
+  Future<Either<Failure, String>> downloadShipmentReport(
+      {required ShipmentReportEntity shipmentReportEntity});
+}
