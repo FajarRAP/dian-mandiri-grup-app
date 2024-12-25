546d0123c25970aa1f3d0d9a17a0eee32cf87573
FajarRiansyah <fajary781@gmail.com>
2024-12-25 10:44:47 +0700

    feat (router): add listener to refresh token

--- lib/core/common/scaffold_with_bottom_navigation_bar.dart
+++ lib/core/common/scaffold_with_bottom_navigation_bar.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../../features/auth/presentation/cubit/auth_cubit.dart';
+import '../../features/tracker/presentation/cubit/shipment_cubit.dart';
+import '../../service_container.dart';
+import 'constants.dart';
 
 class ScaffoldWithBottomNavigationBar extends StatelessWidget {
   final StatefulNavigationShell child;
@@ -9,23 +17,45 @@ class ScaffoldWithBottomNavigationBar extends StatelessWidget {
     required this.child,
   });
 
+  void listener<T>(BuildContext context, T state) async {
+    final storage = getIt.get<FlutterSecureStorage>();
+
+    final refresh = await storage.read(key: refreshTokenKey);
+    if (refresh == null) await GoogleSignIn().signOut();
+    if (!context.mounted) return;
+
+    if (refresh == null) context.go(loginRoute);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: child,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: child.currentIndex,
-        onTap: child.goBranch,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline_outlined),
-            label: 'Profile',
-          ),
-        ],
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<AuthCubit, AuthState>(
+          bloc: context.read<AuthCubit>(),
+          listener: listener<AuthState>,
+        ),
+        BlocListener<ShipmentCubit, ShipmentState>(
+          bloc: context.read<ShipmentCubit>(),
+          listener: listener<ShipmentState>,
+        ),
+      ],
+      child: Scaffold(
+        body: child,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: child.currentIndex,
+          onTap: child.goBranch,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline_outlined),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
     );
   }
