e6b8508ceb6e3989a72a0974919a3b171598434c
FajarRiansyah <fajary781@gmail.com>
2024-12-25 20:19:21 +0700

    refactor (create shipment report use case): migrated from supabase complete

---
+++ lib/features/tracker/domain/usecases/create_shipment_report_use_case.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/common/use_cases.dart';
+import '../../../../core/failure/failure.dart';
+import '../repositories/shipment_repository.dart';
+
+class CreateShipmentReportUseCase
+    implements AsyncUseCaseParams<String, CreateShipmentReportParams> {
+  const CreateShipmentReportUseCase({required this.shipmentRepository});
+
+  final ShipmentRepository shipmentRepository;
+
+  @override
+  Future<Either<Failure, String>> call(
+      CreateShipmentReportParams params) async {
+    return await shipmentRepository.createShipmentReport(
+        startDate: params.startDate, endDate: params.endDate);
+  }
+}
+
+class CreateShipmentReportParams {
+  const CreateShipmentReportParams({
+    required this.startDate,
+    required this.endDate,
+  });
+
+  final String startDate;
+  final String endDate;
+}
