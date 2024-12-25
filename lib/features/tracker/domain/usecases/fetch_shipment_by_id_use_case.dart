872cc1c34d63842b640bf3369ae0c28da8fdd952
FajarRiansyah <fajary781@gmail.com>
2024-12-25 20:19:39 +0700

    feat: add fetch shipment by id use case

---
+++ lib/features/tracker/domain/usecases/fetch_shipment_by_id_use_case.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/common/use_cases.dart';
+import '../../../../core/failure/failure.dart';
+import '../entities/shipment_detail_entity.dart';
+import '../repositories/shipment_repository.dart';
+
+class FetchShipmentByIdUseCase
+    implements AsyncUseCaseParams<ShipmentDetailEntity, String> {
+  const FetchShipmentByIdUseCase({required this.shipmentRepository});
+
+  final ShipmentRepository shipmentRepository;
+
+  @override
+  Future<Either<Failure, ShipmentDetailEntity>> call(String params) async {
+    return await shipmentRepository.fetchShipmentById(shipmentId: params);
+  }
+}
