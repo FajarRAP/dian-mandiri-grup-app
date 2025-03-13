0063b3e10c5336c9914a854dbbcec0aad0d67763
FajarRiansyah <fajary781@gmail.com>
2025-03-14 00:45:56 +0700

    feat (helpers): add evaluateStage method

--- lib/core/helpers/helpers.dart
+++ lib/core/helpers/helpers.dart
@@ -1,6 +1,26 @@
 import 'package:intl/intl.dart';
+import '../common/constants.dart';
 
 final dateFormat = DateFormat('y-MM-dd', 'id_ID');
 final dateTimeFormat = DateFormat('dd-MM-y HH:mm:ss', 'id_ID');
 final dMyFormat = DateFormat('dd-MM-y', 'id_ID');
 final timeFormat = DateFormat('HH:mm:ss', 'id_ID');
+
+String evaluateStage(String stage) {
+  switch (stage) {
+    case scanStage:
+      return 'Scan';
+    case pickUpStage:
+      return 'Ambil Barang';
+    case checkStage:
+      return 'Checker';
+    case packStage:
+      return 'Packing';
+    case sendStage:
+      return 'Kirim';
+    case returnStage:
+      return 'Retur';
+    default:
+      return 'Tidak Diketahui';
+  }
+}
