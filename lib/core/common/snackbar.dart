c6d4d53134bce17ab410338942defcbbf4dc0c7b
Fajar Riansyah <fajary781@gmail.com>
2025-04-25 22:08:41 +0700

    feat (snackbar): add optional margin parameter to success and danger snackbars

--- lib/core/common/snackbar.dart
+++ lib/core/common/snackbar.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
-SnackBar successSnackbar(String message) => SnackBar(
+SnackBar successSnackbar(String message, [EdgeInsetsGeometry? margin]) =>
+    SnackBar(
       backgroundColor: Colors.green,
       behavior: SnackBarBehavior.floating,
       content: Text(message),
       duration: const Duration(milliseconds: 1500),
+      margin: margin,
     );
 
-SnackBar dangerSnackbar(String message) => SnackBar(
+SnackBar dangerSnackbar(String message, [EdgeInsetsGeometry? margin]) =>
+    SnackBar(
       backgroundColor: Colors.red,
       behavior: SnackBarBehavior.floating,
       content: Text(message),
       duration: const Duration(milliseconds: 1500),
+      margin: margin,
     );
