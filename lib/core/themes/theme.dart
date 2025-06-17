65d1529b243151d4f8312fec0f89766fdcf6b0ac
Fajar Riansyah <fajary781@gmail.com>
2025-06-17 23:19:37 +0700

    feat: enhance theme styles and update font settings

--- lib/core/themes/theme.dart
+++ lib/core/themes/theme.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
+const fontFamily = 'Inter18pt';
+
 final theme = ThemeData(
   appBarTheme: const AppBarTheme(
     titleTextStyle: TextStyle(
       color: Colors.black,
+      fontFamily: fontFamily,
       fontSize: 24,
       fontWeight: FontWeight.w600,
     ),
   ),
   colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+  fontFamily: fontFamily,
   inputDecorationTheme: InputDecorationTheme(
     border: OutlineInputBorder(
       borderRadius: BorderRadius.circular(10),
@@ -17,52 +21,67 @@ final theme = ThemeData(
       color: Colors.grey[400],
     ),
   ),
-  useMaterial3: true,
   textTheme: const TextTheme(
+    displayLarge: TextStyle(
+      color: Colors.black,
+      fontSize: 57,
+    ),
+    displayMedium: TextStyle(
+      color: Colors.black,
+      fontSize: 45,
+    ),
+    displaySmall: TextStyle(
+      color: Colors.black,
+      fontSize: 36,
+    ),
     headlineLarge: TextStyle(
       color: Colors.black,
-      fontSize: 24,
-      fontWeight: FontWeight.w600,
+      fontSize: 32,
     ),
     headlineMedium: TextStyle(
       color: Colors.black,
-      fontSize: 22,
-      fontWeight: FontWeight.w600,
+      fontSize: 28,
     ),
     headlineSmall: TextStyle(
       color: Colors.black,
-      fontSize: 20,
-      fontWeight: FontWeight.w600,
+      fontSize: 24,
+    ),
+    titleLarge: TextStyle(
+      color: Colors.black,
+      fontSize: 22,
+    ),
+    titleMedium: TextStyle(
+      color: Colors.black,
+      fontSize: 16,
+    ),
+    titleSmall: TextStyle(
+      color: Colors.black,
+      fontSize: 14,
     ),
     bodyLarge: TextStyle(
       color: Colors.black,
       fontSize: 16,
-      fontWeight: FontWeight.w400,
     ),
     bodyMedium: TextStyle(
       color: Colors.black,
       fontSize: 14,
-      fontWeight: FontWeight.w400,
     ),
     bodySmall: TextStyle(
       color: Colors.black,
       fontSize: 12,
-      fontWeight: FontWeight.w400,
     ),
-    titleLarge: TextStyle(
+    labelLarge: TextStyle(
       color: Colors.black,
-      fontSize: 17,
-      fontWeight: FontWeight.w500,
+      fontSize: 14,
     ),
-    titleMedium: TextStyle(
+    labelMedium: TextStyle(
       color: Colors.black,
-      fontSize: 15,
-      fontWeight: FontWeight.w500,
+      fontSize: 12,
     ),
-    titleSmall: TextStyle(
+    labelSmall: TextStyle(
       color: Colors.black,
-      fontSize: 13,
-      fontWeight: FontWeight.w500,
+      fontSize: 11,
     ),
   ),
+  useMaterial3: true,
 );
