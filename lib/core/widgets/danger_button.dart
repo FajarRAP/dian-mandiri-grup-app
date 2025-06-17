6d1ed95005a9abc54c971ffefd157405702e7585
Fajar Riansyah <fajary781@gmail.com>
2025-06-17 21:52:53 +0700

    feat: add danger button widget

---
+++ lib/core/widgets/danger_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../common/colors.dart';
+
+class DangerButton extends StatelessWidget {
+  const DangerButton({
+    super.key,
+    required this.child,
+    this.height,
+    this.width,
+    this.onPressed,
+  });
+
+  final Widget child;
+  final double? height;
+  final double? width;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: danger,
+        foregroundColor: neutral,
+        fixedSize: Size(
+          width ?? MediaQuery.sizeOf(context).width,
+          height ?? 48,
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 14),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        textStyle: theme.textTheme.bodyMedium?.copyWith(
+          fontWeight: FontWeight.w700,
+          height: 1.4,
+          letterSpacing: .25,
+        ),
+      ),
+      child: child,
+    );
+  }
+}
