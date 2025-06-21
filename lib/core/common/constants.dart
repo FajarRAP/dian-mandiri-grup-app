bbb577ba83d2277081920b9525426b0f3808fab7
Fajar Riansyah <fajary781@gmail.com>
2025-06-21 23:57:33 +0700

    feat: add svgs and register it to constants

--- lib/core/common/constants.dart
+++ lib/core/common/constants.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 
+const homeRoute = "/";
+
 const loginRoute = "/login";
-const profileRoute = "/profile";
 const displayPictureRoute = "/picture";
 
-const trackerRoute = "/";
-const scanReceiptRoute = "${trackerRoute}scan";
-const pickUpReceiptRoute = "${trackerRoute}pick-up";
-const checkReceiptRoute = "${trackerRoute}check";
-const packReceiptRoute = "${trackerRoute}pack";
-const sendReceiptRoute = "${trackerRoute}send";
-const returnReceiptRoute = "${trackerRoute}return";
-const cancelReceiptRoute = "${trackerRoute}cancel";
-const reportRoute = "${trackerRoute}report";
-const receiptStatusRoute = "${trackerRoute}status";
-const detailReceiptRoute = "${trackerRoute}detail";
-const cameraRoute = "${trackerRoute}camera";
+const profileRoute = "${homeRoute}profile";
+
+const trackerRoute = "${homeRoute}tracker";
+const scanReceiptRoute = "$trackerRoute/scan";
+const pickUpReceiptRoute = "$trackerRoute/pick-up";
+const checkReceiptRoute = "$trackerRoute/check";
+const packReceiptRoute = "$trackerRoute/pack";
+const sendReceiptRoute = "$trackerRoute/send";
+const returnReceiptRoute = "$trackerRoute/return";
+const cancelReceiptRoute = "$trackerRoute/cancel";
+const reportRoute = "$trackerRoute/report";
+const receiptStatusRoute = "$trackerRoute/status";
+const detailReceiptRoute = "$trackerRoute/detail";
+const cameraRoute = "$trackerRoute/camera";
 
 const iconsPath = "assets/icons";
 const spreadsheetIcon = "$iconsPath/excel.png";
@@ -29,10 +32,28 @@ const cancelReceiptIcon = "$iconsPath/cancel.png";
 const reportReceiptIcon = "$iconsPath/report.png";
 const checkReceiptStatusIcon = "$iconsPath/check.png";
 const googleIcon = "$iconsPath/google.png";
+
 const soundsPath = "sounds";
 const successSound = "$soundsPath/success.mp3";
 const skipSound = "$soundsPath/skip.mp3";
 const repeatSound = "$soundsPath/repeat.mp3";
+
+const svgPath = 'assets/svgs';
+const boxPen = '$svgPath/box-pen.svg';
+const box = '$svgPath/box.svg';
+const cart = '$svgPath/cart.svg';
+const categorySquare = '$svgPath/category-square.svg';
+const category = '$svgPath/category.svg';
+const dollar = '$svgPath/dollar.svg';
+const personMenu = '$svgPath/person-menu.svg';
+const personOutline = '$svgPath/person-outline.svg';
+const person = '$svgPath/person.svg';
+const reportV1 = '$svgPath/report-v1.svg';
+const reportV2 = '$svgPath/report-v2.svg';
+const reportV3 = '$svgPath/report-v3.svg';
+const reportV4 = '$svgPath/report-v4.svg';
+const reportV5 = '$svgPath/report-v5.svg';
+const warehouse = '$svgPath/warehouse.svg';
 
 const scanStage = 'scan';
 const pickUpStage = 'pick_up';
