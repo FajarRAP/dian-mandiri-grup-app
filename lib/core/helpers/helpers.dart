e5adde1d8a233c276f1c3a6c50e4d81394b0305b
Fajar Riansyah <fajary781@gmail.com>
2025-06-22 02:14:49 +0700

    feat (helpers): add idr currency formatter

--- lib/core/helpers/helpers.dart
+++ lib/core/helpers/helpers.dart
@@ -8,6 +8,11 @@ final dateFormat = DateFormat('y-MM-dd', 'id_ID');
 final dateTimeFormat = DateFormat('dd-MM-y HH:mm:ss', 'id_ID');
 final dMyFormat = DateFormat('dd-MM-y', 'id_ID');
 final timeFormat = DateFormat('HH:mm:ss', 'id_ID');
+final idrCurrencyFormat = NumberFormat.currency(
+  locale: 'id_ID',
+  symbol: 'Rp ',
+  decimalDigits: 0,
+);
 
 String evaluateStage(String stage) {
   switch (stage) {
