41e415db676548ef67ad722a3a8af70a4b4cb8bf
Fajar Riansyah <fajary781@gmail.com>
2025-06-21 19:10:10 +0700

    feat: add new color scheme

--- lib/core/themes/colors.dart
+++ lib/core/themes/colors.dart
@@ -1,4 +1,38 @@
 import 'package:flutter/material.dart';
 
+class MaterialColors {
+  static const primary = Color(0xFF4F5B92);
+  static const onPrimary = Color(0xFFFFFFFF);
+  static const primaryContainer = Color(0xFFDDE1FF);
+  static const onPrimaryContainer = Color(0xFF374379);
+  static const secondary = Color(0xFF5A5D72);
+  static const onSecondary = Color(0xFFFFFFFF);
+  static const secondaryContainer = Color(0xFFDFE1F9);
+  static const onSecondaryContainer = Color(0xFF424659);
+  static const tertiary = Color(0xFF76546E);
+  static const onTertiary = Color(0xFFFFFFFF);
+  static const tertiaryContainer = Color(0xFFFFD7F3);
+  static const onTertiaryContainer = Color(0xFF5C3D56);
+  static const error = Color(0xFFBA1A1A);
+  static const onError = Color(0xFFFFFFFF);
+  static const errorContainer = Color(0xFFFFDAD6);
+  static const onErrorContainer = Color(0xFF93000A);
+  static const surface = Color(0xFFFBF8FF);
+  static const onSurface = Color(0xFF1B1B21);
+}
+
+class CustomColors {
+  static const primaryLight = Color(0xFFEDF1FF);
+  static const primaryLightHover = Color(0xFFE4EAFF);
+  static const primaryLightActive = Color(0xFFC8D3FF);
+  static const primaryNormal = Color(0xFF4E71FF);
+  static const primaryNormalHover = Color(0xFF4666E6);
+  static const primaryNormalActive = Color(0xFF3E5ACC);
+  static const primaryDark = Color(0xFF3B55BF);
+  static const primaryDarkHover = Color(0xFF2F4499);
+  static const primaryDarkActive = Color(0xFF233373);
+  static const primaryDarker = Color(0xFF1B2859);
+}
+
 const neutral = Color(0xFFF3F3F3);
 const danger = Color(0xFFEC221F);
