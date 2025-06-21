688ebb15a84aae466b671026d9f9a1ac4a50293d
Fajar Riansyah <fajary781@gmail.com>
2025-06-22 02:49:40 +0700

    feat (supplier): add supplier repositories

---
+++ lib/features/supplier/domain/repositories/supplier_repositories.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/common/dropdown_entity.dart';
+import '../../../../core/failure/failure.dart';
+import '../entities/supplier_detail_entity.dart';
+import '../entities/supplier_entity.dart';
+
+abstract class SupplierRepositories {
+  Future<Either<Failure, SupplierDetailEntity>> fetchSupplier(
+      {required String supplierId});
+  Future<Either<Failure, List<SupplierEntity>>> fetchSuppliers({
+    String column = 'name',
+    String order = 'asc',
+    String? search,
+    int limit = 10,
+    int page = 1,
+  });
+  Future<Either<Failure, List<DropdownEntity>>> fetchSuppliersDropdown({
+    String? search,
+    int limit = 10,
+    int page = 1,
+  });
+  Future<Either<Failure, SupplierDetailEntity>> insertSupplier(
+      {required SupplierDetailEntity supplierDetailEntity});
+  Future<Either<Failure, SupplierDetailEntity>> updateSupplier(
+      {required SupplierDetailEntity supplierDetailEntity});
+}
