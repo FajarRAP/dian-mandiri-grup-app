305fab88dbe3fa98b6f2a6bd4f176c364cc0aa95
Fajar Riansyah <fajary781@gmail.com>
2025-06-23 00:08:08 +0700

    feat (supplier item): add navigation to edit supplier page

--- lib/features/supplier/presentation/widgets/supplier_item.dart
+++ lib/features/supplier/presentation/widgets/supplier_item.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../core/common/constants.dart';
 import '../../../../core/themes/colors.dart';
+import '../../domain/entities/supplier_entity.dart';
 
 class SupplierItem extends StatelessWidget {
   const SupplierItem({
     super.key,
+    required this.supplier,
   });
+
+  final SupplierEntity supplier;
 
   @override
   Widget build(BuildContext context) {
@@ -19,25 +25,28 @@ class SupplierItem extends StatelessWidget {
       elevation: 1,
       child: ListTile(
         leading: CircleAvatar(
-          backgroundColor: CustomColors.primaryNormal,
-          child: Text(
-            'S1',
-            style: const TextStyle(color: Colors.white),
-          ),
+          backgroundImage: NetworkImage(supplier.avatarUrl),
         ),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10),
         ),
         subtitle: Text(
-          'Alamat Supplier 1',
+          supplier.phoneNumber,
           style: textTheme.bodySmall?.copyWith(
             color: MaterialColors.onSurfaceVariant,
           ),
         ),
         tileColor: MaterialColors.onPrimary,
         title: Text(
-          'Supplier 1',
+          supplier.name,
           style: textTheme.bodyMedium,
+        ),
+        trailing: IconButton(
+          onPressed: () => context.push(
+            editSupplierRoute,
+            extra: supplier.id,
+          ),
+          icon: const Icon(Icons.edit),
         ),
       ),
     );
