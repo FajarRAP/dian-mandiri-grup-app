5f0fc2c064fe249051cdffa51ce17001034833f3
Fajar Riansyah <fajary781@gmail.com>
2025-06-23 00:06:22 +0700

    feat (supplier): add edit supplier page

---
+++ lib/features/supplier/presentation/pages/edit_supplier_page.dart
@@ -0,0 +1,116 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../../../../core/common/shadows.dart';
+import '../../../../core/themes/colors.dart';
+import '../../../../core/widgets/primary_button.dart';
+
+class EditSupplierPage extends StatefulWidget {
+  const EditSupplierPage({
+    super.key,
+    required this.supplierId,
+  });
+
+  final String supplierId;
+
+  @override
+  State<EditSupplierPage> createState() => _EditSupplierPageState();
+}
+
+class _EditSupplierPageState extends State<EditSupplierPage> {
+  late final ImagePicker _imagePicker;
+  XFile? _pickedImage;
+
+  @override
+  initState() {
+    super.initState();
+    _imagePicker = ImagePicker();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        title: const Text('Sunting Supplier'),
+      ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: cardBoxShadow,
+            color: MaterialColors.onPrimary,
+          ),
+          margin: const EdgeInsets.all(16),
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () async {
+                  final pickedImage = await _imagePicker.pickImage(
+                    source: ImageSource.gallery,
+                  );
+
+                  setState(() => _pickedImage = pickedImage);
+                },
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey[300],
+                  backgroundImage: _pickedImage == null
+                      ? null
+                      : FileImage(File(_pickedImage!.path)),
+                  radius: 50,
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: CustomColors.primaryNormal,
+                        shape: BoxShape.circle,
+                      ),
+                      padding: const EdgeInsets.all(4),
+                      child: const Icon(
+                        Icons.edit,
+                        color: MaterialColors.onPrimary,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Nama',
+                ),
+              ),
+              const SizedBox(height: 12),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                ),
+              ),
+              const SizedBox(height: 12),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Telepon',
+                ),
+              ),
+              const SizedBox(height: 12),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Alamat',
+                ),
+              ),
+              const SizedBox(height: 24),
+              PrimaryButton(
+                onPressed: () {},
+                child: const Text('Simpan'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
