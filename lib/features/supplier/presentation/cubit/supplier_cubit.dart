6ea3c2d9c4eddc30d5ac67c1cbbc7a2c3463155d
Fajar Riansyah <fajary781@gmail.com>
2025-06-23 00:07:18 +0700

    feat (supplier): add new state for each use case

--- lib/features/supplier/presentation/cubit/supplier_cubit.dart
+++ lib/features/supplier/presentation/cubit/supplier_cubit.dart
@@ -1,7 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+import '../../../../core/common/dropdown_entity.dart';
 import '../../domain/entities/supplier_detail_entity.dart';
+import '../../domain/entities/supplier_entity.dart';
 import '../../domain/usecases/fetch_supplier_use_case.dart';
 import '../../domain/usecases/fetch_suppliers_dropdown_use_case.dart';
 import '../../domain/usecases/fetch_suppliers_use_case.dart';
@@ -31,7 +33,14 @@ class SupplierCubit extends Cubit<SupplierState> {
   final UpdateSupplierUseCase _updateSupplierUseCase;
 
   Future<void> fetchSupplierUseCase({required String supplierId}) async {
+    emit(FetchSupplierLoading());
+
     final result = await _fetchSupplierUseCase(supplierId);
+
+    result.fold(
+      (l) => emit(FetchSupplierError(message: l.message)),
+      (r) => emit(FetchSupplierLoaded(supplierDetail: r)),
+    );
   }
 
   Future<void> fetchSuppliersUseCase({
@@ -39,28 +48,55 @@ class SupplierCubit extends Cubit<SupplierState> {
     String column = 'name',
     String sort = 'asc',
   }) async {
+    emit(FetchSuppliersLoading());
+
     final result = await _fetchSuppliersUseCase({
       'search': search,
       'column': column,
       'sort': sort,
     });
+
+    result.fold(
+      (l) => emit(FetchSuppliersError(message: l.message)),
+      (r) => emit(FetchSuppliersLoaded(suppliers: r)),
+    );
   }
 
   Future<void> fetchSuppliersDropdownUseCase({
     String? search,
   }) async {
+    emit(FetchSuppliersDropdownLoading());
     final result = await _fetchSuppliersDropdownUseCase({
       'search': search,
     });
+
+    result.fold(
+      (l) => emit(FetchSuppliersDropdownError(message: l.message)),
+      (r) => emit(FetchSuppliersDropdownLoaded(suppliers: r)),
+    );
   }
 
   Future<void> insertSupplierUseCase(
       {required SupplierDetailEntity supplierDetailEntity}) async {
+    emit(InsertSupplierLoading());
+
     final result = await _insertSupplierUseCase(supplierDetailEntity);
+
+    result.fold(
+      (l) => emit(InsertSupplierError(message: l.message)),
+      (r) => emit(InsertSupplierLoaded(supplierDetail: r)),
+    );
   }
 
   Future<void> updateSupplierUseCase(
       {required SupplierDetailEntity supplierDetailEntity}) async {
+    emit(UpdateSupplierLoading());
+
     final result = await _updateSupplierUseCase(supplierDetailEntity);
+
+    result.fold(
+      (l) => emit(UpdateSupplierError(message: l.message)),
+      (r) => emit(UpdateSupplierLoaded(supplierDetail: r)),
+    );
   }
 }
--- lib/features/supplier/presentation/cubit/supplier_state.dart
+++ lib/features/supplier/presentation/cubit/supplier_state.dart
@@ -4,3 +4,83 @@ part of 'supplier_cubit.dart';
 sealed class SupplierState {}
 
 final class SupplierInitial extends SupplierState {}
+
+class FetchSupplier extends SupplierState {}
+
+class FetchSupplierLoading extends FetchSupplier {}
+
+class FetchSupplierLoaded extends FetchSupplier {
+  final SupplierDetailEntity supplierDetail;
+
+  FetchSupplierLoaded({required this.supplierDetail});
+}
+
+class FetchSupplierError extends FetchSupplier {
+  final String message;
+
+  FetchSupplierError({required this.message});
+}
+
+class FetchSuppliers extends SupplierState {}
+
+class FetchSuppliersLoading extends FetchSuppliers {}
+
+class FetchSuppliersLoaded extends FetchSuppliers {
+  final List<SupplierEntity> suppliers;
+
+  FetchSuppliersLoaded({required this.suppliers});
+}
+
+class FetchSuppliersError extends FetchSuppliers {
+  final String message;
+
+  FetchSuppliersError({required this.message});
+}
+
+class FetchSuppliersDropdown extends SupplierState {}
+
+class FetchSuppliersDropdownLoading extends FetchSuppliersDropdown {}
+
+class FetchSuppliersDropdownLoaded extends FetchSuppliersDropdown {
+  final List<DropdownEntity> suppliers;
+
+  FetchSuppliersDropdownLoaded({required this.suppliers});
+}
+
+class FetchSuppliersDropdownError extends FetchSuppliersDropdown {
+  final String message;
+
+  FetchSuppliersDropdownError({required this.message});
+}
+
+class InsertSupplier extends SupplierState {}
+
+class InsertSupplierLoading extends InsertSupplier {}
+
+class InsertSupplierLoaded extends InsertSupplier {
+  final SupplierDetailEntity supplierDetail;
+
+  InsertSupplierLoaded({required this.supplierDetail});
+}
+
+class InsertSupplierError extends InsertSupplier {
+  final String message;
+
+  InsertSupplierError({required this.message});
+}
+
+class UpdateSupplier extends SupplierState {}
+
+class UpdateSupplierLoading extends UpdateSupplier {}
+
+class UpdateSupplierLoaded extends UpdateSupplier {
+  final SupplierDetailEntity supplierDetail;
+
+  UpdateSupplierLoaded({required this.supplierDetail});
+}
+
+class UpdateSupplierError extends UpdateSupplier {
+  final String message;
+
+  UpdateSupplierError({required this.message});
+}
