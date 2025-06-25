db8fb3ba0d79da182ac69b950c0273a04be4b2ac
Fajar Riansyah <fajary781@gmail.com>
2025-06-25 21:00:28 +0700

    feat (supplier repositories impl): implement mock class

--- lib/features/supplier/data/repositories/supplier_repositories_impl.dart
+++ lib/features/supplier/data/repositories/supplier_repositories_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/common/dropdown_entity.dart';
@@ -6,6 +8,8 @@ import '../../domain/entities/supplier_detail_entity.dart';
 import '../../domain/entities/supplier_entity.dart';
 import '../../domain/repositories/supplier_repositories.dart';
 import '../datasources/supplier_remote_data_sources.dart';
+import '../models/supplier_detail_model.dart';
+import '../models/supplier_model.dart';
 
 class SupplierRepositoriesImpl extends SupplierRepositories {
   SupplierRepositoriesImpl({required this.supplierRemoteDataSources});
@@ -14,9 +18,17 @@ class SupplierRepositoriesImpl extends SupplierRepositories {
 
   @override
   Future<Either<Failure, SupplierDetailEntity>> fetchSupplier(
-      {required String supplierId}) {
-    // TODO: implement fetchSupplier
-    throw UnimplementedError();
+      {required String supplierId}) async {
+    try {
+      await Future.delayed(const Duration(milliseconds: 1800));
+      final response =
+          await supplierRemoteDataSources.fetchSupplier(supplierId: supplierId);
+      final data = Map<String, dynamic>.from(jsonDecode(response));
+
+      return Right(SupplierDetailModel.fromJson(data));
+    } catch (e) {
+      return const Left(Failure());
+    }
   }
 
   @override
@@ -25,29 +37,67 @@ class SupplierRepositoriesImpl extends SupplierRepositories {
       String order = 'asc',
       String? search,
       int limit = 10,
-      int page = 1}) {
-    // TODO: implement fetchSuppliers
-    throw UnimplementedError();
+      int page = 1}) async {
+    try {
+      await Future.delayed(const Duration(milliseconds: 1800));
+      final response = await supplierRemoteDataSources.fetchSuppliers();
+      final datas = List<Map<String, dynamic>>.from(jsonDecode(response));
+
+      return Right(datas.map(SupplierModel.fromJson).toList());
+    } catch (e) {
+      return const Left(Failure());
+    }
   }
 
   @override
   Future<Either<Failure, List<DropdownEntity>>> fetchSuppliersDropdown(
-      {String? search, int limit = 10, int page = 1}) {
-    // TODO: implement fetchSuppliersDropdown
-    throw UnimplementedError();
+      {String? search, int limit = 10, int page = 1}) async {
+    try {
+      await Future.delayed(const Duration(milliseconds: 1800));
+      final response = await supplierRemoteDataSources.fetchSuppliersDropdown();
+      final datas = List<Map<String, dynamic>>.from(jsonDecode(response));
+
+      return Right(datas.map(DropdownEntity.fromJson).toList());
+    } catch (e) {
+      return const Left(Failure());
+    }
   }
 
   @override
   Future<Either<Failure, SupplierDetailEntity>> insertSupplier(
-      {required SupplierDetailEntity supplierDetailEntity}) {
-    // TODO: implement insertSupplier
-    throw UnimplementedError();
+      {required SupplierDetailEntity supplierDetailEntity}) async {
+    try {
+      await Future.delayed(const Duration(milliseconds: 1800));
+      final supplierDetail =
+          SupplierDetailModel.fromEntity(supplierDetailEntity);
+      final payload = supplierDetail.toJsonWithAvatar();
+      final response =
+          await supplierRemoteDataSources.insertSupplier(data: payload);
+      final data = Map<String, dynamic>.from(jsonDecode(response));
+
+      return Right(SupplierDetailModel.fromJson(data));
+    } catch (e) {
+      return const Left(Failure());
+    }
   }
 
   @override
   Future<Either<Failure, SupplierDetailEntity>> updateSupplier(
-      {required SupplierDetailEntity supplierDetailEntity}) {
-    // TODO: implement updateSupplier
-    throw UnimplementedError();
+      {required SupplierDetailEntity supplierDetailEntity}) async {
+    try {
+      await Future.delayed(const Duration(milliseconds: 1800));
+      final supplierDetail =
+          SupplierDetailModel.fromEntity(supplierDetailEntity);
+      final payload = supplierDetail.avatarUrl.startsWith('https://')
+          ? supplierDetail.toJsonWithoutAvatar()
+          : supplierDetail.toJsonWithAvatar();
+      final response =
+          await supplierRemoteDataSources.updateSupplier(data: payload);
+      final data = Map<String, dynamic>.from(jsonDecode(response));
+
+      return Right(SupplierDetailModel.fromJson(data));
+    } catch (e) {
+      return const Left(Failure());
+    }
   }
 }
