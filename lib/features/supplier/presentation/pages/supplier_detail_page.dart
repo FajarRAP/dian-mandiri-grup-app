d87f252ad6c4bff689d0c112fbe618e9fd4fdccf
Fajar Riansyah <fajary781@gmail.com>
2025-06-25 21:02:30 +0700

    feat (supplier detail page): simulate fetch supplier detail use case

--- lib/features/supplier/presentation/pages/supplier_detail_page.dart
+++ lib/features/supplier/presentation/pages/supplier_detail_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/common/shadows.dart';
 import '../../../../core/themes/colors.dart';
+import '../cubit/supplier_cubit.dart';
 
 class SupplierDetailPage extends StatelessWidget {
   const SupplierDetailPage({
@@ -13,62 +15,81 @@ class SupplierDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final supplierCubit = context.read<SupplierCubit>();
+
     return Scaffold(
-      resizeToAvoidBottomInset: true,
       appBar: AppBar(title: const Text('Detail Supplier')),
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: cardBoxShadow,
-            color: MaterialColors.onPrimary,
-          ),
-          margin: const EdgeInsets.all(16),
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundColor: Colors.grey[300],
-                backgroundImage: NetworkImage('supplierDetail.avatarUrl'),
-                radius: 50,
-              ),
-              const SizedBox(height: 24),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Nama',
+      body: BlocBuilder<SupplierCubit, SupplierState>(
+        bloc: supplierCubit..fetchSupplier(supplierId: supplierId),
+        buildWhen: (previous, current) => current is FetchSupplier,
+        builder: (context, state) {
+          if (state is FetchSupplierLoading) {
+            return const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          }
+
+          if (state is FetchSupplierLoaded) {
+            return Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: cardBoxShadow,
+                  color: MaterialColors.onPrimary,
                 ),
-                initialValue: 'Dimas',
-                readOnly: true,
-              ),
-              const SizedBox(height: 12),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Email',
+                margin: const EdgeInsets.all(16),
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.grey[300],
+                      backgroundImage:
+                          NetworkImage(state.supplierDetail.avatarUrl),
+                      radius: 50,
+                    ),
+                    const SizedBox(height: 24),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Nama',
+                      ),
+                      initialValue: state.supplierDetail.name,
+                      readOnly: true,
+                    ),
+                    const SizedBox(height: 12),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Email',
+                      ),
+                      initialValue: state.supplierDetail.email,
+                      readOnly: true,
+                    ),
+                    const SizedBox(height: 12),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Telepon',
+                      ),
+                      initialValue: state.supplierDetail.phoneNumber,
+                      readOnly: true,
+                    ),
+                    const SizedBox(height: 12),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Alamat',
+                      ),
+                      initialValue: state.supplierDetail.address,
+                      readOnly: true,
+                    ),
+                  ],
                 ),
-                initialValue: 'dimas@gmail.com',
-                readOnly: true,
               ),
-              const SizedBox(height: 12),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Telepon',
-                ),
-                initialValue: '087826474787',
-                readOnly: true,
-              ),
-              const SizedBox(height: 12),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Alamat',
-                ),
-                initialValue: 'Kapling',
-                readOnly: true,
-              ),
-            ],
-          ),
-        ),
+            );
+          }
+
+          return const SizedBox();
+        },
       ),
+      resizeToAvoidBottomInset: true,
     );
   }
 }
