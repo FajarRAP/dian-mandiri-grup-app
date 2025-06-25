da31d8d744f92a6b28fd1157eb54598537256e96
Fajar Riansyah <fajary781@gmail.com>
2025-06-25 21:00:00 +0700

    feat (supplier remote data sources): add mock class

--- lib/features/supplier/data/datasources/supplier_remote_data_sources.dart
+++ lib/features/supplier/data/datasources/supplier_remote_data_sources.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
 
 abstract class SupplierRemoteDataSources<T> {
   Future<T> fetchSupplier({required String supplierId});
@@ -58,5 +59,44 @@ class SupplierRemoteDataSourcesImpl
   Future<Response> updateSupplier({required Map<String, dynamic> data}) {
     // TODO: implement updateSupplier
     throw UnimplementedError();
+  }
+}
+
+class SupplierRemoteDataSourcesMock
+    implements SupplierRemoteDataSources<String> {
+  @override
+  Future<String> fetchSupplier({required String supplierId}) async {
+    return await rootBundle.loadString('dummy_json/supplier.json');
+  }
+
+  @override
+  Future<String> fetchSuppliers(
+      {String column = 'name',
+      String order = 'asc',
+      String? search,
+      int limit = 10,
+      int page = 1}) async {
+    return await rootBundle.loadString('dummy_json/suppliers.json');
+  }
+
+  @override
+  Future<String> fetchSuppliersDropdown(
+      {String? search, int limit = 10, int page = 1}) async {
+    return await rootBundle.loadString('dummy_json/suppliers_dropdown.json');
+  }
+
+  @override
+  Future<String> insertSupplier({required Map<String, dynamic> data}) async {
+    return await rootBundle.loadString('dummy_json/supplier.json');
+  }
+
+  @override
+  Future<String> updateSupplier({required Map<String, dynamic> data}) async {
+    if (data['avatar'] != null) {
+      // Multipart
+      return await rootBundle.loadString('dummy_json/supplier.json');
+    } else {
+      return await rootBundle.loadString('dummy_json/supplier.json');
+    }
   }
 }
