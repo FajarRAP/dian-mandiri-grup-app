1edd7d5b4aa616a1a0819778ff6a092f1960c09d
Fajar Riansyah <fajary781@gmail.com>
2025-07-07 03:25:58 +0700

    feat (add shipping fee): simulate insert shipping fee use case

--- lib/features/warehouse/presentation/pages/add_shipping_fee_page.dart
+++ lib/features/warehouse/presentation/pages/add_shipping_fee_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../../core/common/constants.dart';
 import '../../../../core/common/dropdown_entity.dart';
+import '../../../../core/common/snackbar.dart';
+import '../../../../core/helpers/validators.dart';
 import '../../../../core/themes/colors.dart';
 import '../../../../core/widgets/dropdown_modal_item.dart';
 import '../../../../core/widgets/dropdown_search_modal.dart';
 import '../../../../core/widgets/fab_container.dart';
 import '../../../../core/widgets/primary_button.dart';
+import '../cubit/warehouse_cubit.dart';
+import '../widgets/selected_purchase_note_item.dart';
 
 class AddShippingFeePage extends StatefulWidget {
   const AddShippingFeePage({
@@ -18,7 +24,24 @@ class AddShippingFeePage extends StatefulWidget {
 }
 
 class _AddShippingFeePageState extends State<AddShippingFeePage> {
+  late final WarehouseCubit _warehouseCubit;
+  late final TextEditingController _shippingFeeController;
+  late final GlobalKey<FormState> _formKey;
   final _selectedPurchaseNoteIds = <DropdownEntity>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _warehouseCubit = context.read<WarehouseCubit>();
+    _formKey = GlobalKey<FormState>();
+    _shippingFeeController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _shippingFeeController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,104 +52,146 @@ class _AddShippingFeePageState extends State<AddShippingFeePage> {
       appBar: AppBar(
         title: const Text('Tambah Ongkos Kirim'),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(16),
-        children: <Widget>[
-          Text(
-            'Harga Ongkos Kirim',
-            style: textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 8),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: 'Harga Ongkos Kirim',
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: <Widget>[
+            Text(
+              'Harga Ongkos Kirim',
+              style: textTheme.bodyLarge,
             ),
-            keyboardType: TextInputType.number,
-          ),
-          const SizedBox(height: 24),
-          Text(
-            'Pilih Nota',
-            style: textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 8),
-          TextField(
-            onTap: () => showModalBottomSheet(
-              context: context,
-              builder: (context) => DropdownSearchModal(
-                search: (keyword) {},
-                title: 'nota',
-                child: Expanded(
-                  child: ListView.separated(
-                    itemBuilder: (context, index) => GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _selectedPurchaseNoteIds.add(
-                            DropdownEntity(
-                              key: '$index',
-                              value: 'Nota $index',
-                            ),
+            const SizedBox(height: 8),
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              controller: _shippingFeeController,
+              decoration: InputDecoration(
+                hintText: 'Harga Ongkos Kirim',
+              ),
+              keyboardType: TextInputType.number,
+              validator: nullValidator,
+            ),
+            const SizedBox(height: 24),
+            Text(
+              'Pilih Nota',
+              style: textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              onTap: () => showModalBottomSheet(
+                context: context,
+                builder: (context) => DropdownSearchModal(
+                  search: (keyword) => _warehouseCubit
+                      .fetchPurchaseNotesDropdown(search: keyword),
+                  title: 'nota',
+                  child: Expanded(
+                    child: BlocBuilder<WarehouseCubit, WarehouseState>(
+                      bloc: _warehouseCubit..fetchPurchaseNotesDropdown(),
+                      buildWhen: (previous, current) =>
+                          current is FetchPurchaseNotesDropdown,
+                      builder: (context, state) {
+                        if (state is FetchPurchaseNotesDropdownLoading) {
+                          return const Center(
+                            child: CircularProgressIndicator.adaptive(),
                           );
-                        });
-                        context.pop();
+                        }
+
+                        if (state is FetchPurchaseNotesDropdownLoaded) {
+                          return ListView.separated(
+                            itemBuilder: (context, index) => GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _selectedPurchaseNoteIds
+                                      .add(state.purchaseNotes[index]);
+                                });
+                                context.pop();
+                              },
+                              child: DropdownModalItem(
+                                child: Text(
+                                  state.purchaseNotes[index].value,
+                                  style: textTheme.titleMedium?.copyWith(
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            separatorBuilder: (context, index) =>
+                                const SizedBox(height: 12),
+                            itemCount: state.purchaseNotes.length,
+                          );
+                        }
+
+                        return const SizedBox();
                       },
-                      child: DropdownModalItem(
-                        child: Text(
-                          'Nota $index',
-                          style: textTheme.titleMedium?.copyWith(
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
                     ),
-                    separatorBuilder: (context, index) =>
-                        const SizedBox(height: 12),
-                    itemCount: 10,
                   ),
                 ),
               ),
-            ),
-            decoration: InputDecoration(
-              hintText: 'Pilih nota',
-              suffixIcon: const Icon(Icons.arrow_drop_down),
-            ),
-            readOnly: true,
-          ),
-          const SizedBox(height: 24),
-          Divider(color: MaterialColors.outlineVariant),
-          const SizedBox(height: 24),
-          ListView.separated(
-            itemBuilder: (context, index) => Material(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
+              decoration: InputDecoration(
+                hintText: 'Pilih Nota',
+                suffixIcon: const Icon(Icons.arrow_drop_down),
               ),
-              elevation: 1,
-              child: ListTile(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                tileColor: MaterialColors.surfaceContainerLowest,
-                title: Text(_selectedPurchaseNoteIds[index].value),
-                trailing: IconButton(
-                  onPressed: () =>
-                      setState(() => _selectedPurchaseNoteIds.removeAt(index)),
-                  icon: const Icon(Icons.close),
-                ),
-              ),
+              readOnly: true,
             ),
-            separatorBuilder: (context, index) => const SizedBox(height: 12),
-            itemCount: _selectedPurchaseNoteIds.length,
-            physics: const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-          ),
-        ],
+            const SizedBox(height: 24),
+            Divider(color: MaterialColors.outlineVariant),
+            const SizedBox(height: 24),
+            ListView.separated(
+              itemBuilder: (context, index) => SelectedPurchaseNoteItem(
+                onDelete: () =>
+                    setState(() => _selectedPurchaseNoteIds.removeAt(index)),
+                title: _selectedPurchaseNoteIds[index].value,
+              ),
+              separatorBuilder: (context, index) => const SizedBox(height: 12),
+              itemCount: _selectedPurchaseNoteIds.length,
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FABContainer(
-        child: PrimaryButton(
-          onPressed: () {},
-          child: const Text('Simpan'),
+        child: BlocConsumer<WarehouseCubit, WarehouseState>(
+          buildWhen: (previous, current) => current is InsertShippingFee,
+          listenWhen: (previous, current) => current is InsertShippingFee,
+          listener: (context, state) {
+            if (state is InsertShippingFeeLoaded) {
+              scaffoldMessengerKey.currentState?.showSnackBar(
+                successSnackbar(state.message),
+              );
+              context.pop();
+              _warehouseCubit.fetchPurchaseNotes();
+            }
+          },
+          builder: (context, state) {
+            if (state is InsertShippingFeeLoading) {
+              return PrimaryButton(child: const Text('Simpan'));
+            }
+
+            return PrimaryButton(
+              onPressed: () async {
+                if (!_formKey.currentState!.validate()) return;
+
+                if (_selectedPurchaseNoteIds.isEmpty) {
+                  scaffoldMessengerKey.currentState?.showSnackBar(
+                    dangerSnackbar('Pilih minimal 1 nota'),
+                  );
+                  return;
+                }
+
+                _warehouseCubit.insertShippingFee(
+                  price: int.parse(_shippingFeeController.text),
+                  purchaseNoteIds:
+                      _selectedPurchaseNoteIds.map((e) => e.key).toList(),
+                );
+              },
+              child: const Text('Simpan'),
+            );
+          },
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      resizeToAvoidBottomInset: false,
     );
   }
 }
