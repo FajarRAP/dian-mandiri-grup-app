1f7fd75da140af868622792ca9d845238fcf2ed8
Fajar Riansyah <fajary781@gmail.com>
2025-07-10 02:30:17 +0700

    feat (failure): add spreadsheet failure class

--- lib/core/failure/failure.dart
+++ lib/core/failure/failure.dart
@@ -7,3 +7,29 @@ class Failure {
   final int statusCode;
   final String message;
 }
+
+class SpreadsheetFailure extends Failure {
+  const SpreadsheetFailure({
+    super.statusCode,
+    super.message,
+    required this.headers,
+    required this.hiddenColumnCount,
+    required this.rows,
+  });
+
+  final List<String> headers;
+  final int hiddenColumnCount;
+  final List<List<Map<String, dynamic>?>> rows;
+
+  factory SpreadsheetFailure.fromJson(Map<String, dynamic> json) {
+    return SpreadsheetFailure(
+      statusCode: json['statusCode'] as int? ?? 500,
+      message: json['message'] as String? ?? 'Terjadi kesalahan',
+      headers: List<String>.from(json['header']),
+      hiddenColumnCount: json['hide_column'],
+      rows: List<dynamic>.from(json['content'])
+          .map((e) => List<Map<String, dynamic>?>.from(e))
+          .toList(),
+    );
+  }
+}
