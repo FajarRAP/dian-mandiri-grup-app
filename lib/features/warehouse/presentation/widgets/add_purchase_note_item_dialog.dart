9f607c4d6c1b094122bafe347644d6c071301804
Fajar Riansyah <fajary781@gmail.com>
2025-07-10 01:24:10 +0700

    feat (purchase note item dialogs): refactor to use Form and add validation for input fields

--- lib/features/warehouse/presentation/widgets/add_purchase_note_item_dialog.dart
+++ lib/features/warehouse/presentation/widgets/add_purchase_note_item_dialog.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/helpers/validators.dart';
 import '../../../../core/widgets/primary_button.dart';
+import '../../domain/entities/warehouse_item_entity.dart';
 
-class AddPurchaseNoteItemDialog extends StatelessWidget {
+class AddPurchaseNoteItemDialog extends StatefulWidget {
   const AddPurchaseNoteItemDialog({
     super.key,
     required this.onTap,
   });
 
-  final void Function() onTap;
+  final void Function(WarehouseItemEntity item) onTap;
+
+  @override
+  State<AddPurchaseNoteItemDialog> createState() =>
+      _AddPurchaseNoteItemDialogState();
+}
+
+class _AddPurchaseNoteItemDialogState extends State<AddPurchaseNoteItemDialog> {
+  late final GlobalKey<FormState> _formKey;
+  late final TextEditingController _nameController;
+  late final TextEditingController _priceController;
+  late final TextEditingController _totalController;
+  late final TextEditingController _totalRejectController;
+
+  @override
+  void initState() {
+    super.initState();
+    _formKey = GlobalKey<FormState>();
+    _nameController = TextEditingController();
+    _priceController = TextEditingController();
+    _totalController = TextEditingController();
+    _totalRejectController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _priceController.dispose();
+    _totalController.dispose();
+    _totalRejectController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,59 +50,87 @@ class AddPurchaseNoteItemDialog extends StatelessWidget {
 
     return AlertDialog(
       contentPadding: const EdgeInsets.all(24),
-      content: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(
-            'Nama',
-            style: textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 4),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: 'Nama Barang',
+      content: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'Nama',
+              style: textTheme.bodyLarge,
             ),
-          ),
-          const SizedBox(height: 12),
-          Text(
-            'Harga per Barang',
-            style: textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 4),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: 'Harga per Barang',
+            const SizedBox(height: 4),
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              controller: _nameController,
+              decoration: InputDecoration(
+                hintText: 'Nama Barang',
+              ),
+              validator: nullValidator,
             ),
-          ),
-          const SizedBox(height: 12),
-          Text(
-            'Total Barang (Pcs)',
-            style: textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 4),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: 'Total Barang (Pcs)',
+            const SizedBox(height: 12),
+            Text(
+              'Harga per Barang',
+              style: textTheme.bodyLarge,
             ),
-          ),
-          const SizedBox(height: 12),
-          Text(
-            'Total Barang Reject (Pcs)',
-            style: textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 4),
-          TextFormField(
-            decoration: InputDecoration(
-              hintText: 'Total Barang Reject (Pcs)',
+            const SizedBox(height: 4),
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              controller: _priceController,
+              decoration: InputDecoration(
+                hintText: 'Harga per Barang',
+              ),
+              keyboardType: TextInputType.number,
+              validator: nullValidator,
             ),
-          ),
-          const SizedBox(height: 24),
-          PrimaryButton(
-            onPressed: onTap,
-            child: const Text('Simpan'),
-          ),
-        ],
+            const SizedBox(height: 12),
+            Text(
+              'Total Barang (Pcs)',
+              style: textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 4),
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              controller: _totalController,
+              decoration: InputDecoration(
+                hintText: 'Total Barang (Pcs)',
+              ),
+              keyboardType: TextInputType.number,
+              validator: nullValidator,
+            ),
+            const SizedBox(height: 12),
+            Text(
+              'Total Barang Reject (Pcs)',
+              style: textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 4),
+            TextFormField(
+              controller: _totalRejectController,
+              decoration: InputDecoration(
+                hintText: 'Total Barang Reject (Pcs)',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 24),
+            PrimaryButton(
+              onPressed: () {
+                if (!_formKey.currentState!.validate()) return;
+
+                final item = WarehouseItemEntity(
+                  name: _nameController.text,
+                  price: int.parse(_priceController.text),
+                  quantity: int.parse(_totalController.text),
+                  rejectQuantity:
+                      int.tryParse(_totalRejectController.text) ?? 0,
+                );
+
+                widget.onTap(item);
+              },
+              child: const Text('Simpan'),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/features/warehouse/presentation/widgets/edit_purchase_note_item_dialog.dart
+++ lib/features/warehouse/presentation/widgets/edit_purchase_note_item_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/helpers/validators.dart';
 import '../../../../core/widgets/primary_button.dart';
 import '../../domain/entities/warehouse_item_entity.dart';
 
@@ -20,6 +21,7 @@ class EditPurchaseNoteItemDialog extends StatefulWidget {
 
 class _EditPurchaseNoteItemDialogState
     extends State<EditPurchaseNoteItemDialog> {
+  late final GlobalKey<FormState> _formKey;
   late final TextEditingController _nameController;
   late final TextEditingController _priceController;
   late final TextEditingController _quantityController;
@@ -28,6 +30,7 @@ class _EditPurchaseNoteItemDialogState
   @override
   void initState() {
     super.initState();
+    _formKey = GlobalKey<FormState>();
     _nameController =
         TextEditingController(text: widget.warehouseItemEntity.name);
     _priceController =
@@ -54,71 +57,85 @@ class _EditPurchaseNoteItemDialogState
 
     return AlertDialog(
       contentPadding: const EdgeInsets.all(24),
-      content: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(
-            'Nama',
-            style: textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 4),
-          TextFormField(
-            controller: _nameController,
-            decoration: InputDecoration(
-              hintText: 'Nama Barang',
+      content: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'Nama',
+              style: textTheme.bodyLarge,
             ),
-          ),
-          const SizedBox(height: 12),
-          Text(
-            'Harga per Barang',
-            style: textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 4),
-          TextFormField(
-            controller: _priceController,
-            decoration: InputDecoration(
-              hintText: 'Harga per Barang',
+            const SizedBox(height: 4),
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              controller: _nameController,
+              decoration: InputDecoration(
+                hintText: 'Nama Barang',
+              ),
+              validator: nullValidator,
             ),
-          ),
-          const SizedBox(height: 12),
-          Text(
-            'Total Barang (Pcs)',
-            style: textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 4),
-          TextFormField(
-            controller: _quantityController,
-            decoration: InputDecoration(
-              hintText: 'Total Barang (Pcs)',
+            const SizedBox(height: 12),
+            Text(
+              'Harga per Barang',
+              style: textTheme.bodyLarge,
             ),
-          ),
-          const SizedBox(height: 12),
-          Text(
-            'Total Barang Reject (Pcs)',
-            style: textTheme.bodyLarge,
-          ),
-          const SizedBox(height: 4),
-          TextFormField(
-            controller: _rejectController,
-            decoration: InputDecoration(
-              hintText: 'Total Barang Reject (Pcs)',
+            const SizedBox(height: 4),
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              controller: _priceController,
+              decoration: InputDecoration(
+                hintText: 'Harga per Barang',
+              ),
+              keyboardType: TextInputType.number,
+              validator: nullValidator,
             ),
-          ),
-          const SizedBox(height: 24),
-          PrimaryButton(
-            onPressed: () {
-              final edited = WarehouseItemEntity(
-                name: _nameController.text,
-                quantity: int.tryParse(_quantityController.text) ?? 0,
-                rejectQuantity: int.tryParse(_rejectController.text) ?? 0,
-                price: int.tryParse(_priceController.text) ?? 0,
-              );
-              widget.onTap(edited);
-            },
-            child: const Text('Simpan'),
-          ),
-        ],
+            const SizedBox(height: 12),
+            Text(
+              'Total Barang (Pcs)',
+              style: textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 4),
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              controller: _quantityController,
+              decoration: InputDecoration(
+                hintText: 'Total Barang (Pcs)',
+              ),
+              keyboardType: TextInputType.number,
+              validator: nullValidator,
+            ),
+            const SizedBox(height: 12),
+            Text(
+              'Total Barang Reject (Pcs)',
+              style: textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 4),
+            TextFormField(
+              controller: _rejectController,
+              decoration: InputDecoration(
+                hintText: 'Total Barang Reject (Pcs)',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 24),
+            PrimaryButton(
+              onPressed: () {
+                if (!_formKey.currentState!.validate()) return;
+
+                final edited = WarehouseItemEntity(
+                  name: _nameController.text,
+                  price: int.parse(_priceController.text),
+                  quantity: int.parse(_quantityController.text),
+                  rejectQuantity: int.tryParse(_rejectController.text) ?? 0,
+                );
+                widget.onTap(edited);
+              },
+              child: const Text('Simpan'),
+            ),
+          ],
+        ),
       ),
     );
   }
