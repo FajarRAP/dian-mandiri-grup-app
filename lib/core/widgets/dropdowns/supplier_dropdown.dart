65f566606c089602adc5afad0dc3c0b8a408a245
Fajar Riansyah <fajary781@gmail.com>
2025-07-10 01:24:26 +0700

    feat: add supplier dropdown widget

---
+++ lib/core/widgets/dropdowns/supplier_dropdown.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../features/supplier/presentation/cubit/supplier_cubit.dart';
+import '../../common/dropdown_entity.dart';
+import '../dropdown_modal_item.dart';
+import '../dropdown_search_modal.dart';
+
+class SupplierDropdown extends StatelessWidget {
+  const SupplierDropdown({
+    super.key,
+    required this.onTap,
+  });
+
+  final void Function(DropdownEntity supplier) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final supplierCubit = context.read<SupplierCubit>();
+
+    return DropdownSearchModal(
+      search: (keyword) =>
+          supplierCubit.fetchSuppliersDropdown(search: keyword),
+      title: 'supplier',
+      child: Expanded(
+        child: BlocBuilder<SupplierCubit, SupplierState>(
+          bloc: supplierCubit..fetchSuppliersDropdown(),
+          buildWhen: (previous, current) => current is FetchSuppliersDropdown,
+          builder: (context, state) {
+            if (state is FetchSuppliersDropdownLoading) {
+              return const Center(
+                child: CircularProgressIndicator.adaptive(),
+              );
+            }
+
+            if (state is FetchSuppliersDropdownLoaded) {
+              return ListView.separated(
+                itemBuilder: (context, index) => GestureDetector(
+                  onTap: () => onTap(state.suppliers[index]),
+                  child: DropdownModalItem(
+                    child: Text(state.suppliers[index].value),
+                  ),
+                ),
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 12),
+                itemCount: state.suppliers.length,
+              );
+            }
+
+            return const SizedBox();
+          },
+        ),
+      ),
+    );
+  }
+}
