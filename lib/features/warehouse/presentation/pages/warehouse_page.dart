69dd24ec5841114fe271322f77c6c92299f40501
Fajar Riansyah <fajary781@gmail.com>
2025-08-23 11:16:00 +0700

    feat (warehouse page): refactor to implement pagination for purchase notes and enhance search functionality

--- lib/features/warehouse/presentation/pages/warehouse_page.dart
+++ lib/features/warehouse/presentation/pages/warehouse_page.dart
@@ -18,9 +18,9 @@ class WarehousePage extends StatefulWidget {
 }
 
 class _WarehousePageState extends State<WarehousePage> {
-  late final WarehouseCubit _warehouseCubit;
   late final Debouncer _debouncer;
-  late final ScrollController _scrollController;
+  late final FocusNode _focusNode;
+  late final WarehouseCubit _warehouseCubit;
   var _column = 'created_at';
   var _order = 'asc';
   String? _search;
@@ -28,15 +28,13 @@ class _WarehousePageState extends State<WarehousePage> {
   @override
   void initState() {
     super.initState();
-    _warehouseCubit = context.read<WarehouseCubit>()..fetchPurchaseNotes();
     _debouncer = Debouncer(delay: const Duration(milliseconds: 500));
-    _scrollController = ScrollController();
-    _scrollController.addListener(_onLastItem);
+    _focusNode = FocusScope.of(context, createDependency: false);
+    _warehouseCubit = context.read<WarehouseCubit>()..fetchPurchaseNotes();
   }
 
   @override
   void dispose() {
-    _scrollController.dispose();
     _debouncer.cancel();
     super.dispose();
   }
@@ -46,136 +44,149 @@ class _WarehousePageState extends State<WarehousePage> {
     return Scaffold(
       body: RefreshIndicator(
         onRefresh: _warehouseCubit.fetchPurchaseNotes,
-        child: CustomScrollView(
-          controller: _scrollController,
-          physics: const AlwaysScrollableScrollPhysics(),
-          slivers: <Widget>[
-            // App Bar
-            SliverAppBar(
-              backgroundColor: MaterialColors.surfaceContainerLowest,
-              floating: true,
-              pinned: true,
-              snap: true,
-              title: const Text('Barang Masuk'),
-              actions: <Widget>[
-                PopupMenuButton(
-                  onSelected: (value) {
-                    final params = value.split(',');
-                    _column = params.first;
-                    _order = params.last;
-                    _warehouseCubit.fetchPurchaseNotes(
-                      column: _column,
-                      order: _order,
-                      search: _search,
-                    );
-                  },
-                  icon: const Icon(Icons.sort),
-                  tooltip: 'Urutkan',
-                  itemBuilder: (context) => const [
-                    PopupMenuItem(
-                      value: 'created_at,asc',
-                      child: Text('Tanggal Naik'),
-                    ),
-                    PopupMenuItem(
-                      value: 'created_at,desc',
-                      child: Text('Tanggal Turun'),
-                    ),
-                    PopupMenuItem(
-                      value: 'total_item,asc',
-                      child: Text('Barang Naik'),
-                    ),
-                    PopupMenuItem(
-                      value: 'total_item,desc',
-                      child: Text('Barang Turun'),
-                    ),
-                  ],
-                ),
-              ],
-              bottom: PreferredSize(
-                preferredSize: const Size.fromHeight(88),
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: TextFormField(
-                    onChanged: (value) {
-                      _search = value;
-                      _debouncer.run(() => _warehouseCubit.fetchPurchaseNotes(
-                          column: _column, order: _order, search: _search));
+        child: NotificationListener<ScrollNotification>(
+          onNotification: (scrollState) {
+            if (scrollState.runtimeType == ScrollEndNotification &&
+                _warehouseCubit.state is! ListPaginateLast) {
+              _warehouseCubit.fetchPurchaseNotesPaginate(
+                column: _column,
+                order: _order,
+                search: _search,
+              );
+            }
+
+            return false;
+          },
+          child: CustomScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            slivers: <Widget>[
+              // App Bar
+              SliverAppBar(
+                backgroundColor: MaterialColors.surfaceContainerLowest,
+                floating: true,
+                pinned: true,
+                snap: true,
+                title: const Text('Barang Masuk'),
+                actions: <Widget>[
+                  PopupMenuButton(
+                    onSelected: (value) {
+                      final params = value.split(',');
+                      _column = params.first;
+                      _order = params.last;
+                      _warehouseCubit.fetchPurchaseNotes(
+                        column: _column,
+                        order: _order,
+                        search: _search,
+                      );
                     },
-                    onTapOutside: (event) => FocusScope.of(context).unfocus(),
-                    decoration: const InputDecoration(
-                      hintText: 'Cari Nota',
-                      prefixIcon: Icon(Icons.search),
+                    icon: const Icon(Icons.sort),
+                    tooltip: 'Urutkan',
+                    itemBuilder: (context) => const [
+                      PopupMenuItem(
+                        value: 'created_at,asc',
+                        child: Text('Tanggal Naik'),
+                      ),
+                      PopupMenuItem(
+                        value: 'created_at,desc',
+                        child: Text('Tanggal Turun'),
+                      ),
+                      PopupMenuItem(
+                        value: 'total_item,asc',
+                        child: Text('Barang Naik'),
+                      ),
+                      PopupMenuItem(
+                        value: 'total_item,desc',
+                        child: Text('Barang Turun'),
+                      ),
+                    ],
+                  ),
+                ],
+                bottom: PreferredSize(
+                  preferredSize: const Size.fromHeight(88),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: TextFormField(
+                      onChanged: (value) {
+                        _search = value;
+                        _debouncer.run(() => _warehouseCubit.fetchPurchaseNotes(
+                            column: _column, order: _order, search: _search));
+                      },
+                      onTapOutside: (event) => _focusNode.unfocus(),
+                      decoration: const InputDecoration(
+                        hintText: 'Cari Nota',
+                        prefixIcon: Icon(Icons.search),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
 
-            // List
-            BlocBuilder<WarehouseCubit, WarehouseState>(
-              bloc: _warehouseCubit,
-              buildWhen: (previous, current) => current is FetchPurchaseNotes,
-              builder: (context, state) {
-                if (state is FetchPurchaseNotesLoading) {
-                  return const SliverFillRemaining(
-                    child: Center(
-                      child: CircularProgressIndicator.adaptive(),
-                    ),
-                  );
-                }
-
-                if (state is FetchPurchaseNotesLoaded) {
-                  if (state.purchaseNotes.isEmpty) {
+              // List
+              BlocBuilder<WarehouseCubit, WarehouseState>(
+                bloc: _warehouseCubit,
+                buildWhen: (previous, current) => current is FetchPurchaseNotes,
+                builder: (context, state) {
+                  if (state is FetchPurchaseNotesLoading) {
                     return const SliverFillRemaining(
                       child: Center(
-                        child: Text('Belum ada nota'),
+                        child: CircularProgressIndicator.adaptive(),
                       ),
                     );
                   }
 
-                  return SliverPadding(
-                    padding: const EdgeInsets.all(16),
-                    sliver: SliverList.separated(
-                      itemBuilder: (context, index) => GestureDetector(
-                        onTap: () => context.push(
-                          purchaseNoteDetailRoute,
-                          extra: _warehouseCubit.purchaseNotes[index].id,
+                  if (state is FetchPurchaseNotesLoaded) {
+                    if (state.purchaseNotes.isEmpty) {
+                      return const SliverFillRemaining(
+                        child: Center(
+                          child: Text('Belum ada nota'),
                         ),
-                        child: PurchaseNoteItem(
-                          purchaseNoteSummary:
-                              _warehouseCubit.purchaseNotes[index],
+                      );
+                    }
+
+                    return SliverPadding(
+                      padding: const EdgeInsets.all(16),
+                      sliver: SliverList.separated(
+                        itemBuilder: (context, index) => GestureDetector(
+                          onTap: () => context.push(
+                            purchaseNoteDetailRoute,
+                            extra: _warehouseCubit.purchaseNotes[index].id,
+                          ),
+                          child: PurchaseNoteItem(
+                            purchaseNoteSummary:
+                                _warehouseCubit.purchaseNotes[index],
+                          ),
+                        ),
+                        separatorBuilder: (context, index) =>
+                            const SizedBox(height: 12),
+                        itemCount: _warehouseCubit.purchaseNotes.length,
+                      ),
+                    );
+                  }
+
+                  return const SliverToBoxAdapter();
+                },
+              ),
+
+              // Widget when Pagination
+              BlocBuilder<WarehouseCubit, WarehouseState>(
+                buildWhen: (previous, current) => current is ListPaginate,
+                builder: (context, state) {
+                  if (state is ListPaginateLoading) {
+                    return SliverPadding(
+                      padding: const EdgeInsets.all(16),
+                      sliver: const SliverToBoxAdapter(
+                        child: Center(
+                          child: CircularProgressIndicator.adaptive(),
                         ),
                       ),
-                      separatorBuilder: (context, index) =>
-                          const SizedBox(height: 12),
-                      itemCount: _warehouseCubit.purchaseNotes.length,
-                    ),
-                  );
-                }
+                    );
+                  }
 
-                return const SliverToBoxAdapter();
-              },
-            ),
-           
-            // Widget when Pagination
-            BlocBuilder<WarehouseCubit, WarehouseState>(
-              buildWhen: (previous, current) => current is ListPaginate,
-              builder: (context, state) {
-                if (state is ListPaginateLoading) {
-                  return SliverPadding(
-                    padding: const EdgeInsets.all(16),
-                    sliver: const SliverToBoxAdapter(
-                      child: Center(
-                        child: CircularProgressIndicator.adaptive(),
-                      ),
-                    ),
-                  );
-                }
-
-                return const SliverToBoxAdapter();
-              },
-            ),
-          ],
+                  return const SliverToBoxAdapter();
+                },
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: ExpandableFAB(
@@ -197,19 +208,5 @@ class _WarehousePageState extends State<WarehousePage> {
       ),
       resizeToAvoidBottomInset: false,
     );
-  }
-
-  void _onLastItem() {
-    final currentPosition = _scrollController.position.pixels;
-    final lastItemPosition = _scrollController.position.maxScrollExtent;
-    final isLastPosition = currentPosition >= lastItemPosition;
-
-    if (isLastPosition && _warehouseCubit.state is! ListPaginateLast) {
-      _warehouseCubit.fetchPurchaseNotesPaginate(
-        column: _column,
-        order: _order,
-        search: _search,
-      );
-    }
   }
 }
