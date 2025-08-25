6c171cedaaee41bab2cf3d8705915a1ccddda5e3
Fajar Riansyah <fajary781@gmail.com>
2025-08-25 12:10:06 +0700

    reafactor (helpers): move TopSnackbar class to separate file

--- lib/core/helpers/helpers.dart
+++ lib/core/helpers/helpers.dart
@@ -1,11 +1,10 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:intl/intl.dart';
+import 'package:ship_tracker/core/failure/failure.dart';
 
 import '../common/constants.dart';
 import '../exceptions/refresh_token.dart';
-import '../themes/colors.dart';
 
 final dateFormat = DateFormat('y-MM-dd', 'id_ID');
 final dateTimeFormat = DateFormat('dd-MM-y HH:mm:ss', 'id_ID');
@@ -47,69 +46,14 @@ bool isRefreshed<T>(T state, String? refreshToken) {
   return state is RefreshToken && refreshToken != null;
 }
 
-class TopSnackbar {
-  static late BuildContext _context;
+List parseSpreadsheetFailure(SpreadsheetFailure spreadsheetFailure) {
+  final headers = spreadsheetFailure.headers
+      .map((e) => DataColumn(label: Text(e)))
+      .toList();
+  final rows = spreadsheetFailure.rows
+      .map((e) => DataRow(
+          cells: e.map((el) => DataCell(Text(el?['value'] ?? ''))).toList()))
+      .toList();
 
-  static void init(BuildContext context) => _context = context;
-
-  static void dangerSnackbar({required String message}) =>
-      _showSnackbar(message: message, color: MaterialColors.error);
-
-  static void successSnackbar({required String message}) =>
-      _showSnackbar(message: message, color: Colors.green);
-
-  static void defaultSnackbar({required String message}) =>
-      _showSnackbar(message: message, color: Colors.grey.shade900);
-
-  static void _showSnackbar({required String message, required Color color}) {
-    final overlay = Overlay.of(_context);
-
-    final overlayEntry = OverlayEntry(
-      builder: (context) => Positioned(
-        top: MediaQuery.paddingOf(context).top + 24,
-        left: 16,
-        right: 16,
-        child: _buildSnackbar(color: color, text: message)
-            .animate()
-            .slideY(begin: -3, end: 0)
-            .then()
-            .slideY(begin: 0.15, end: 0, duration: 200.ms)
-            .then()
-            .slideY(begin: 0, end: 0.15, duration: 200.ms)
-            .then(delay: 1.seconds)
-            .slideY(begin: 0, end: -3),
-      ),
-    );
-
-    overlay.insert(overlayEntry);
-
-    Future.delayed(2.seconds, overlayEntry.remove);
-  }
-
-  static Widget _buildSnackbar({required Color color, required String text}) {
-    return Material(
-      color: Colors.transparent,
-      child: Container(
-        alignment: Alignment.center,
-        constraints: BoxConstraints(minHeight: 48),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: color,
-        ),
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 6,
-        ),
-        child: Text(
-          text,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 14,
-            fontWeight: FontWeight.w500,
-          ),
-          textAlign: TextAlign.center,
-        ),
-      ),
-    );
-  }
+  return [headers, rows];
 }
