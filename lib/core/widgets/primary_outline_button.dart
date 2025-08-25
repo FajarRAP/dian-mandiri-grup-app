c0b5f67c5aa5efef58c9187dce31b38e890e8b8f
Fajar Riansyah <fajary781@gmail.com>
2025-08-25 12:11:26 +0700

    refactor (primary outline button): remove unused width parameter and change to icon constructor

--- lib/core/widgets/primary_outline_button.dart
+++ lib/core/widgets/primary_outline_button.dart
@@ -7,33 +7,31 @@ class PrimaryOutlineButton extends StatelessWidget {
     super.key,
     this.onPressed,
     this.height,
-    this.width,
+    this.icon,
     required this.child,
   });
 
   final void Function()? onPressed;
   final double? height;
-  final double? width;
+  final Widget? icon;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return ElevatedButton.icon(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
         backgroundColor: MaterialColors.onPrimary,
         elevation: 0,
         foregroundColor: CustomColors.primaryNormal,
-        fixedSize: Size(
-          width ?? MediaQuery.sizeOf(context).width,
-          height ?? 48,
-        ),
+        fixedSize: Size.fromHeight(height ?? 40),
         side: BorderSide(
           color: CustomColors.primaryNormal,
           width: 1.5,
         ),
       ),
-      child: child,
+      icon: icon,
+      label: child,
     );
   }
 }
