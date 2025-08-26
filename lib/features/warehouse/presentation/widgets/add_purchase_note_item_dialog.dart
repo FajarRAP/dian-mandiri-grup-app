9d3cdde9e251bb527d913340062676be020d23f9
Fajar Riansyah <fajary781@gmail.com>
2025-08-26 15:50:35 +0700

    feat (purchase note dialogs): refactor to use PurchaseNoteItemDialog for adding and editing items

--- lib/features/warehouse/presentation/widgets/add_purchase_note_item_dialog.dart
+++ lib/features/warehouse/presentation/widgets/add_purchase_note_item_dialog.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../../../../core/helpers/validators.dart';
-import '../../../../core/widgets/primary_button.dart';
 import '../../domain/entities/warehouse_item_entity.dart';
+import 'purchase_note_item_dialog.dart';
 
 class AddPurchaseNoteItemDialog extends StatefulWidget {
   const AddPurchaseNoteItemDialog({
@@ -18,7 +17,6 @@ class AddPurchaseNoteItemDialog extends StatefulWidget {
 }
 
 class _AddPurchaseNoteItemDialogState extends State<AddPurchaseNoteItemDialog> {
-  late final GlobalKey<FormState> _formKey;
   late final TextEditingController _nameController;
   late final TextEditingController _priceController;
   late final TextEditingController _totalController;
@@ -27,7 +25,6 @@ class _AddPurchaseNoteItemDialogState extends State<AddPurchaseNoteItemDialog> {
   @override
   void initState() {
     super.initState();
-    _formKey = GlobalKey<FormState>();
     _nameController = TextEditingController();
     _priceController = TextEditingController();
     _totalController = TextEditingController();
@@ -45,94 +42,13 @@ class _AddPurchaseNoteItemDialogState extends State<AddPurchaseNoteItemDialog> {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
-
-    return AlertDialog(
-      contentPadding: const EdgeInsets.all(24),
-      content: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text(
-              'Nama',
-              style: textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 4),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              controller: _nameController,
-              decoration: const InputDecoration(
-                hintText: 'Nama Barang',
-              ),
-              validator: nullValidator,
-            ),
-            const SizedBox(height: 12),
-            Text(
-              'Harga per Barang',
-              style: textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 4),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              controller: _priceController,
-              decoration: const InputDecoration(
-                hintText: 'Harga per Barang',
-              ),
-              keyboardType: TextInputType.number,
-              validator: nullValidator,
-            ),
-            const SizedBox(height: 12),
-            Text(
-              'Total Barang (Pcs)',
-              style: textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 4),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              controller: _totalController,
-              decoration: const InputDecoration(
-                hintText: 'Total Barang (Pcs)',
-              ),
-              keyboardType: TextInputType.number,
-              validator: nullValidator,
-            ),
-            const SizedBox(height: 12),
-            Text(
-              'Total Barang Reject (Pcs)',
-              style: textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 4),
-            TextFormField(
-              controller: _totalRejectController,
-              decoration: const InputDecoration(
-                hintText: 'Total Barang Reject (Pcs)',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            const SizedBox(height: 24),
-            PrimaryButton(
-              onPressed: () {
-                if (!_formKey.currentState!.validate()) return;
-
-                final item = WarehouseItemEntity(
-                  name: _nameController.text,
-                  price: int.parse(_priceController.text),
-                  quantity: int.parse(_totalController.text),
-                  rejectQuantity:
-                      int.tryParse(_totalRejectController.text) ?? 0,
-                );
-
-                widget.onTap(item);
-              },
-              child: const Text('Simpan'),
-            ),
-          ],
-        ),
-      ),
-      scrollable: true,
+    return PurchaseNoteItemDialog(
+      onTap: widget.onTap,
+      title: 'Tambah Barang',
+      nameController: _nameController,
+      priceController: _priceController,
+      totalController: _totalController,
+      totalRejectController: _totalRejectController,
     );
   }
 }
--- lib/features/warehouse/presentation/widgets/edit_purchase_note_item_dialog.dart
+++ lib/features/warehouse/presentation/widgets/edit_purchase_note_item_dialog.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-import '../../../../core/helpers/validators.dart';
-import '../../../../core/widgets/primary_button.dart';
 import '../../domain/entities/warehouse_item_entity.dart';
+import 'purchase_note_item_dialog.dart';
 
 class EditPurchaseNoteItemDialog extends StatefulWidget {
   const EditPurchaseNoteItemDialog({
@@ -21,23 +20,21 @@ class EditPurchaseNoteItemDialog extends StatefulWidget {
 
 class _EditPurchaseNoteItemDialogState
     extends State<EditPurchaseNoteItemDialog> {
-  late final GlobalKey<FormState> _formKey;
   late final TextEditingController _nameController;
   late final TextEditingController _priceController;
-  late final TextEditingController _quantityController;
-  late final TextEditingController _rejectController;
+  late final TextEditingController _totalController;
+  late final TextEditingController _totalRejectController;
 
   @override
   void initState() {
     super.initState();
-    _formKey = GlobalKey<FormState>();
     _nameController =
         TextEditingController(text: widget.warehouseItemEntity.name);
     _priceController =
         TextEditingController(text: '${widget.warehouseItemEntity.price}');
-    _quantityController =
+    _totalController =
         TextEditingController(text: '${widget.warehouseItemEntity.quantity}');
-    _rejectController = TextEditingController(
+    _totalRejectController = TextEditingController(
         text: '${widget.warehouseItemEntity.rejectQuantity}');
   }
 
@@ -45,99 +42,20 @@ class _EditPurchaseNoteItemDialogState
   void dispose() {
     _nameController.dispose();
     _priceController.dispose();
-    _quantityController.dispose();
-    _rejectController.dispose();
+    _totalController.dispose();
+    _totalRejectController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
-
-    return AlertDialog(
-      contentPadding: const EdgeInsets.all(24),
-      content: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text(
-              'Nama',
-              style: textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 4),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              controller: _nameController,
-              decoration: const InputDecoration(
-                hintText: 'Nama Barang',
-              ),
-              validator: nullValidator,
-            ),
-            const SizedBox(height: 12),
-            Text(
-              'Harga per Barang',
-              style: textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 4),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              controller: _priceController,
-              decoration: const InputDecoration(
-                hintText: 'Harga per Barang',
-              ),
-              keyboardType: TextInputType.number,
-              validator: nullValidator,
-            ),
-            const SizedBox(height: 12),
-            Text(
-              'Total Barang (Pcs)',
-              style: textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 4),
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              controller: _quantityController,
-              decoration: const InputDecoration(
-                hintText: 'Total Barang (Pcs)',
-              ),
-              keyboardType: TextInputType.number,
-              validator: nullValidator,
-            ),
-            const SizedBox(height: 12),
-            Text(
-              'Total Barang Reject (Pcs)',
-              style: textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 4),
-            TextFormField(
-              controller: _rejectController,
-              decoration: const InputDecoration(
-                hintText: 'Total Barang Reject (Pcs)',
-              ),
-              keyboardType: TextInputType.number,
-            ),
-            const SizedBox(height: 24),
-            PrimaryButton(
-              onPressed: () {
-                if (!_formKey.currentState!.validate()) return;
-
-                final edited = WarehouseItemEntity(
-                  name: _nameController.text,
-                  price: int.parse(_priceController.text),
-                  quantity: int.parse(_quantityController.text),
-                  rejectQuantity: int.tryParse(_rejectController.text) ?? 0,
-                );
-                widget.onTap(edited);
-              },
-              child: const Text('Simpan'),
-            ),
-          ],
-        ),
-      ),
-      scrollable: true,
+    return PurchaseNoteItemDialog(
+      onTap: widget.onTap,
+      title: 'Edit Barang',
+      nameController: _nameController,
+      priceController: _priceController,
+      totalController: _totalController,
+      totalRejectController: _totalRejectController,
     );
   }
 }
