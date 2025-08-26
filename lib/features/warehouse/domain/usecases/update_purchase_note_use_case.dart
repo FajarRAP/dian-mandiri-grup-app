cfb2bdc49e5d0ac9599bd21582dfb605b876c718
Fajar Riansyah <fajary781@gmail.com>
2025-08-26 15:17:05 +0700

    refactor (update purchase note use case): change parameter type from InsertPurchaseNoteManualEntity to Map<String, dynamic>

--- lib/features/warehouse/domain/usecases/update_purchase_note_use_case.dart
+++ lib/features/warehouse/domain/usecases/update_purchase_note_use_case.dart
@@ -2,16 +2,19 @@ import 'package:dartz/dartz.dart';
 
 import '../../../../core/common/use_cases.dart';
 import '../../../../core/failure/failure.dart';
-import '../entities/insert_purchase_note_manual_entity.dart';
 import '../repositories/warehouse_repositories.dart';
 
-class UpdatePurchaseNoteUseCase implements AsyncUseCaseParams<String, InsertPurchaseNoteManualEntity>{
+class UpdatePurchaseNoteUseCase
+    implements AsyncUseCaseParams<String, Map<String, dynamic>> {
   const UpdatePurchaseNoteUseCase({required this.warehouseRepositories});
 
   final WarehouseRepositories warehouseRepositories;
 
   @override
-  Future<Either<Failure, String>> call(InsertPurchaseNoteManualEntity params) async {
-    return await warehouseRepositories.updatePurchaseNote(purchaseNote: params);
+  Future<Either<Failure, String>> call(Map<String, dynamic> params) async {
+    return await warehouseRepositories.updatePurchaseNote(
+      purchaseNoteId: params['purchase_note_id'],
+      purchaseNote: params['purchase_note'],
+    );
   }
 }
