51a66c14ab46f6beceb990766da20476787f674f
Fajar Riansyah <fajary781@gmail.com>
2025-08-27 16:37:16 +0700

    feat (supplier page): enhance supplier fetching with pagination and search functionality

--- lib/features/supplier/presentation/pages/supplier_page.dart
+++ lib/features/supplier/presentation/pages/supplier_page.dart
@@ -14,113 +14,158 @@ class SupplierPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final debouncer = Debouncer(delay: const Duration(milliseconds: 500));
-    final supplierCubit = context.read<SupplierCubit>();
+    final supplierCubit = context.read<SupplierCubit>()..fetchSuppliers();
+    final focusNode = FocusScope.of(context, createDependency: false);
     var column = 'name';
     var order = 'asc';
     String? search;
 
     return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          PopupMenuButton(
-            onSelected: (value) {
-              final params = value.split(',');
-              column = params.first;
-              order = params.last;
-              supplierCubit.fetchSuppliers(
-                column: column,
-                order: order,
-                search: search,
-              );
-            },
-            icon: const Icon(Icons.sort),
-            tooltip: 'Urutkan',
-            itemBuilder: (context) => <PopupMenuEntry<String>>[
-              PopupMenuItem(
-                value: 'name,asc',
-                child: const Text('Nama Naik'),
-              ),
-              PopupMenuItem(
-                value: 'name,desc',
-                child: const Text('Nama Turun'),
-              ),
-              PopupMenuItem(
-                value: 'created_at,asc',
-                child: const Text('Tanggal Ditambahkan Naik'),
-              ),
-              PopupMenuItem(
-                value: 'created_at,desc',
-                child: const Text('Tanggal Ditambahkan Turun'),
-              ),
-            ],
-          ),
-        ],
-        backgroundColor: MaterialColors.onPrimary,
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(80),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: TextFormField(
-              onChanged: (value) {
-                search = value;
-                debouncer.run(
-                  () => supplierCubit.fetchSuppliers(
-                    search: search,
-                    column: column,
-                    order: order,
-                  ),
-                );
-              },
-              decoration: InputDecoration(
-                hintText: 'Cari Supplier',
-                prefixIcon: const Icon(Icons.search),
-              ),
-            ),
-          ),
-        ),
-        title: const Text('Supplier'),
-      ),
-      body: RefreshIndicator(
-        onRefresh: supplierCubit.fetchSuppliers,
-        displacement: 10,
-        child: BlocBuilder<SupplierCubit, SupplierState>(
-          bloc: supplierCubit
-            ..fetchSuppliers(
-              column: column,
-              order: order,
-              search: search,
-            ),
-          buildWhen: (previous, current) => current is FetchSuppliers,
-          builder: (context, state) {
-            if (state is FetchSuppliersLoading) {
-              return const Center(
-                child: CircularProgressIndicator.adaptive(),
-              );
-            }
-
-            if (state is FetchSuppliersLoaded) {
-              if (state.suppliers.isEmpty) {
-                return const Center(
-                  child: Text('Belum ada supplier'),
-                );
+      body: BlocListener<SupplierCubit, SupplierState>(
+        listener: (context, state) {
+          if (state is InsertSupplierLoaded || state is UpdateSupplierLoaded) {
+            supplierCubit.fetchSuppliers();
+          }
+        },
+        child: RefreshIndicator(
+          onRefresh: supplierCubit.fetchSuppliers,
+          child: NotificationListener<ScrollNotification>(
+            onNotification: (scrollState) {
+              if (scrollState.runtimeType == ScrollEndNotification &&
+                  supplierCubit.state is! ListPaginateLast) {
+                supplierCubit.fetchSuppliersPaginate(
+                    column: column, order: order, search: search);
               }
 
-              return ListView.separated(
-                itemBuilder: (context, index) => GestureDetector(
-                  onTap: () => context.push(
-                    supplierDetailRoute,
-                    extra: state.suppliers[index].id,
+              return false;
+            },
+            child: CustomScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              slivers: <Widget>[
+                // App Bar
+                SliverAppBar(
+                  actions: <Widget>[
+                    PopupMenuButton(
+                      onSelected: (value) {
+                        final params = value.split(',');
+                        column = params.first;
+                        order = params.last;
+                        supplierCubit.fetchSuppliers(
+                          column: column,
+                          order: order,
+                          search: search,
+                        );
+                      },
+                      icon: const Icon(Icons.sort),
+                      tooltip: 'Urutkan',
+                      itemBuilder: (context) => <PopupMenuEntry<String>>[
+                        PopupMenuItem(
+                          value: 'name,asc',
+                          child: const Text('Nama Naik'),
+                        ),
+                        PopupMenuItem(
+                          value: 'name,desc',
+                          child: const Text('Nama Turun'),
+                        ),
+                        PopupMenuItem(
+                          value: 'created_at,asc',
+                          child: const Text('Tanggal Ditambahkan Naik'),
+                        ),
+                        PopupMenuItem(
+                          value: 'created_at,desc',
+                          child: const Text('Tanggal Ditambahkan Turun'),
+                        ),
+                      ],
+                    ),
+                  ],
+                  backgroundColor: MaterialColors.surfaceContainerLowest,
+                  bottom: PreferredSize(
+                    preferredSize: const Size.fromHeight(88),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: TextFormField(
+                        onChanged: (value) {
+                          search = value;
+                          debouncer.run(() => supplierCubit.fetchSuppliers(
+                              search: search, column: column, order: order));
+                        },
+                        onTapOutside: (event) => focusNode.unfocus(),
+                        decoration: InputDecoration(
+                          hintText: 'Cari Supplier',
+                          prefixIcon: const Icon(Icons.search),
+                        ),
+                      ),
+                    ),
                   ),
-                  child: SupplierItem(supplier: state.suppliers[index]),
+                  floating: true,
+                  pinned: true,
+                  snap: true,
+                  title: const Text('Supplier'),
                 ),
-                separatorBuilder: (context, index) => const SizedBox(height: 6),
-                itemCount: state.suppliers.length,
-                padding: const EdgeInsets.all(16),
-              );
-            }
+                // List
+                BlocBuilder<SupplierCubit, SupplierState>(
+                  bloc: supplierCubit,
+                  buildWhen: (previous, current) => current is FetchSuppliers,
+                  builder: (context, state) {
+                    if (state is FetchSuppliersLoading) {
+                      return const SliverFillRemaining(
+                        child: Center(
+                          child: CircularProgressIndicator.adaptive(),
+                        ),
+                      );
+                    }
 
-            return const SizedBox();
-          },
+                    if (state is FetchSuppliersLoaded) {
+                      if (state.suppliers.isEmpty) {
+                        return const SliverFillRemaining(
+                          child: Center(
+                            child: Text('Belum ada supplier'),
+                          ),
+                        );
+                      }
+
+                      return SliverPadding(
+                        padding: const EdgeInsets.all(16),
+                        sliver: SliverList.separated(
+                          itemBuilder: (context, index) => GestureDetector(
+                            onTap: () => context.push(
+                              supplierDetailRoute,
+                              extra: state.suppliers[index].id,
+                            ),
+                            child:
+                                SupplierItem(supplier: state.suppliers[index]),
+                          ),
+                          separatorBuilder: (context, index) =>
+                              const SizedBox(height: 8),
+                          itemCount: state.suppliers.length,
+                        ),
+                      );
+                    }
+
+                    return const SizedBox();
+                  },
+                ),
+                // Widget when Pagination
+                BlocBuilder<SupplierCubit, SupplierState>(
+                  buildWhen: (previous, current) => current is ListPaginate,
+                  builder: (context, state) {
+                    if (state is ListPaginateLoading) {
+                      return SliverPadding(
+                        padding: const EdgeInsets.all(16),
+                        sliver: const SliverToBoxAdapter(
+                          child: Center(
+                            child: CircularProgressIndicator.adaptive(),
+                          ),
+                        ),
+                      );
+                    }
+
+                    return const SliverToBoxAdapter();
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
