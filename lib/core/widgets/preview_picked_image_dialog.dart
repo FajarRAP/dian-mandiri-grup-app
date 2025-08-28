f48c876a928ed7501914646a7e47109a2417ae6f
Fajar Riansyah <fajary781@gmail.com>
2025-08-28 15:22:15 +0700

    refactor (preview picked image dialog): update border radius for improved aesthetics and consistency

--- lib/core/widgets/preview_picked_image_dialog.dart
+++ lib/core/widgets/preview_picked_image_dialog.dart
@@ -18,21 +18,23 @@ class PreviewPickedImageDialog extends StatelessWidget {
       alignment: Alignment.center,
       contentPadding: EdgeInsets.all(2),
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.zero,
+        borderRadius: BorderRadius.circular(6),
       ),
-      
-      content: pickedImagePath == null
-          ? Text(
-              'Belum memilih gambar',
-              style: textTheme.bodyLarge?.copyWith(
-                fontWeight: FontWeight.w600,
+      content: ClipRRect(
+        borderRadius: BorderRadius.circular(6),
+        child: pickedImagePath == null
+            ? Text(
+                'Belum memilih gambar',
+                style: textTheme.bodyLarge?.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
+              )
+            : Image.file(
+                File(pickedImagePath!),
+                fit: BoxFit.cover,
               ),
-              textAlign: TextAlign.center,
-            )
-          : Image.file(
-              File(pickedImagePath!),
-              fit: BoxFit.cover,
-            ),
+      ),
     );
   }
 }
