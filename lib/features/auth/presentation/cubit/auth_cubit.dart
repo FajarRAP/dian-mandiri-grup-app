8b7731f253c6c6620e33a2ba9604db3d9dd73cf7
Fajar Riansyah <fajary781@gmail.com>
2025-08-29 11:45:51 +0700

    refactor (auth cubit): remove unused auth states and add RefreshTokenExpired for better state management

--- lib/features/auth/presentation/cubit/auth_cubit.dart
+++ lib/features/auth/presentation/cubit/auth_cubit.dart
@@ -1,9 +1,6 @@
 import 'package:bloc/bloc.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:meta/meta.dart';
 
-import '../../../../core/common/constants.dart';
-import '../../../../core/helpers/helpers.dart';
 import '../../domain/entities/user_entity.dart';
 import '../../domain/usecases/fetch_user_from_storage_use_case.dart';
 import '../../domain/usecases/fetch_user_use_case.dart';
@@ -22,14 +19,12 @@ class AuthCubit extends Cubit<AuthState> {
     required SignInUseCase signInUseCase,
     required SignOutUseCase signOutUseCase,
     required UpdateProfileUseCase updateProfileUseCase,
-    required FlutterSecureStorage storage,
   })  : _fetchUserUseCase = fetchUserUseCase,
         _fetchUserFromStorageUseCase = fetchUserFromStorageUseCase,
         _refreshTokenUseCase = refreshTokenUseCase,
         _signInUseCase = signInUseCase,
         _signOutUseCase = signOutUseCase,
         _updateProfileUseCase = updateProfileUseCase,
-        _storage = storage,
         super(AuthInitial());
 
   final FetchUserUseCase _fetchUserUseCase;
@@ -38,20 +33,15 @@ class AuthCubit extends Cubit<AuthState> {
   final SignInUseCase _signInUseCase;
   final SignOutUseCase _signOutUseCase;
   final UpdateProfileUseCase _updateProfileUseCase;
-  final FlutterSecureStorage _storage;
-
   late UserEntity user;
 
   Future<void> fetchUser() async {
     emit(FetchUserLoading());
 
-    final refreshToken = await _storage.read(key: refreshTokenKey);
     final result = await _fetchUserUseCase();
 
     result.fold(
-      (l) => isRefreshed(l, refreshToken)
-          ? fetchUser()
-          : emit(FetchUserError(message: l.message)),
+      (l) => emit(FetchUserError(message: l.message)),
       (r) {
         user = r;
         emit(FetchUserLoaded());
@@ -73,10 +63,9 @@ class AuthCubit extends Cubit<AuthState> {
     );
   }
 
-  Future<void> refreshToken() async {
+  Future<void> refreshToken({required String? refreshToken}) async {
     emit(RefreshTokenLoading());
 
-    final refreshToken = await _storage.read(key: refreshTokenKey);
     final result = await _refreshTokenUseCase('$refreshToken');
 
     result.fold(
@@ -114,14 +103,13 @@ class AuthCubit extends Cubit<AuthState> {
   Future<void> updateProfile({required String name}) async {
     emit(UpdateProfileLoading());
 
-    final refreshToken = await _storage.read(key: refreshTokenKey);
     final result = await _updateProfileUseCase(name);
 
     result.fold(
-      (l) => isRefreshed(l, refreshToken)
-          ? updateProfile(name: name)
-          : emit(UpdateProfileError(message: l.message)),
+      (l) => emit(UpdateProfileError(message: l.message)),
       (r) => emit(UpdateProfileLoaded(message: r)),
     );
   }
+
+  void refreshTokenExpired() => emit(RefreshTokenExpired());
 }
--- lib/features/auth/presentation/cubit/auth_state.dart
+++ lib/features/auth/presentation/cubit/auth_state.dart
@@ -77,40 +77,5 @@ class UpdateProfileError extends UpdateProfile {
   UpdateProfileError({required this.message});
 }
 
-class AuthLoading extends AuthState {}
 
-class AuthLoaded extends AuthState {}
-
-class AuthSignedOut extends AuthState {
-  final String message;
-
-  AuthSignedOut(this.message);
-}
-
-class AuthError extends AuthState {
-  final String message;
-
-  AuthError(this.message);
-}
-
-class UpdatingUser extends AuthState {}
-
-class UserUpdated extends AuthState {}
-
-class UpdateUserError extends AuthState {
-  final String message;
-
-  UpdateUserError(this.message);
-}
-
-class TokenSended extends AuthState {
-  final String message;
-
-  TokenSended(this.message);
-}
-
-class PasswordChanged extends AuthState {
-  final String message;
-
-  PasswordChanged(this.message);
-}
+class RefreshTokenExpired extends AuthState {}
