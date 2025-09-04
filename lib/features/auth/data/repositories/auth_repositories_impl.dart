76324ab6db034b15ca238ed46a4e5bcd0b73b48f
Fajar Riansyah <fajary781@gmail.com>
2025-09-05 00:08:09 +0700

    feat (auth repositories): refactor authentication methods to use local data sources and improve error handling

--- lib/features/auth/data/repositories/auth_repositories_impl.dart
+++ lib/features/auth/data/repositories/auth_repositories_impl.dart
@@ -1,53 +1,75 @@
-import 'dart:convert';
-
 import 'package:dartz/dartz.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
-import '../../../../core/common/constants.dart';
+import '../../../../core/exceptions/cache_exception.dart';
+import '../../../../core/exceptions/internal_exception.dart';
+import '../../../../core/exceptions/server_exception.dart';
 import '../../../../core/failure/failure.dart';
-import '../../../../core/helpers/top_snackbar.dart';
 import '../../domain/entities/user_entity.dart';
 import '../../domain/repositories/auth_repositories.dart';
+import '../datasources/auth_local_data_sources.dart';
 import '../datasources/auth_remote_data_sources.dart';
-import '../models/user_model.dart';
 
 class AuthRepositoriesImpl implements AuthRepositories {
   const AuthRepositoriesImpl({
-    required this.authRemoteDataSource,
-    required this.googleSignIn,
-    required this.storage,
+    required this.authLocalDataSources,
+    required this.authRemoteDataSources,
   });
 
-  final AuthRemoteDataSources<Response> authRemoteDataSource;
-  final GoogleSignIn googleSignIn;
-  final FlutterSecureStorage storage;
+  final AuthLocalDataSources authLocalDataSources;
+  final AuthRemoteDataSources authRemoteDataSources;
 
   @override
   Future<Either<Failure, UserEntity>> fetchUser() async {
     try {
-      final response = await authRemoteDataSource.fetchUser();
+      final result = await authRemoteDataSources.fetchUser();
 
-      return Right(UserModel.fromJson(response.data['data']));
-    } on DioException catch (de) {
-      switch (de.response?.statusCode) {
-        default:
-          return Left(Failure());
-      }
-    } catch (e) {
-      return Left(Failure(message: '$e'));
+      return Right(result);
+    } on ServerException catch (se) {
+      return Left(ServerFailure(
+        message: se.message,
+        statusCode: se.statusCode,
+      ));
+    } on CacheException catch (ce) {
+      return Left(CacheFailure(
+        message: ce.message,
+        statusCode: ce.statusCode,
+      ));
+    } on InternalException catch (ie) {
+      return Left(Failure(
+        message: ie.message,
+        statusCode: ie.statusCode,
+      ));
     }
   }
 
   @override
   Future<Either<Failure, UserEntity>> fetchUserFromStorage() async {
     try {
-      final user = await storage.read(key: userKey);
+      final result = await authLocalDataSources.getUser();
 
-      return Right(UserModel.fromJson(jsonDecode('$user')));
-    } catch (e) {
-      return const Left(Failure());
+      if (result == null) {
+        return Left(CacheFailure(
+          message: 'No user found in storage',
+          statusCode: 404,
+        ));
+      }
+
+      return Right(result);
+    } on ServerException catch (se) {
+      return Left(ServerFailure(
+        message: se.message,
+        statusCode: se.statusCode,
+      ));
+    } on CacheException catch (ce) {
+      return Left(CacheFailure(
+        message: ce.message,
+        statusCode: ce.statusCode,
+      ));
+    } on InternalException catch (ie) {
+      return Left(Failure(
+        message: ie.message,
+        statusCode: ie.statusCode,
+      ));
     }
   }
 
@@ -55,72 +77,101 @@ class AuthRepositoriesImpl implements AuthRepositories {
   Future<Either<Failure, String>> refreshToken(
       {required String refreshToken}) async {
     try {
-      final response =
-          await authRemoteDataSource.refreshToken(refreshToken: refreshToken);
-      final data = response.data['data'];
+      final result =
+          await authRemoteDataSources.refreshToken(refreshToken: refreshToken);
+      await authLocalDataSources.cacheTokens(token: result);
 
-      await storage.write(key: accessTokenKey, value: data['access_token']);
-      await storage.write(key: refreshTokenKey, value: data['refresh_token']);
-
-      return Right(response.data['message']);
-    } catch (e) {
-      return const Left(Failure());
+      return Right(result.message);
+    } on ServerException catch (se) {
+      return Left(ServerFailure(
+        message: se.message,
+        statusCode: se.statusCode,
+      ));
+    } on CacheException catch (ce) {
+      return Left(CacheFailure(
+        message: ce.message,
+        statusCode: ce.statusCode,
+      ));
+    } on InternalException catch (ie) {
+      return Left(Failure(
+        message: ie.message,
+        statusCode: ie.statusCode,
+      ));
     }
   }
 
   @override
-  Future<Either<Failure, UserEntity>> signIn() async {
+  Future<Either<Failure, String>> signIn() async {
     try {
-      final googleSignInAccount = await googleSignIn.signIn();
-      final googleSignInAuthentication =
-          await googleSignInAccount?.authentication;
-      final response = await authRemoteDataSource.signIn(
-          googleAccessToken: '${googleSignInAuthentication?.accessToken}');
-      final data = response.data['data'];
+      final result = await authRemoteDataSources.signIn();
+      await authLocalDataSources.cacheUser(user: result.user);
+      await authLocalDataSources.cacheTokens(token: result.token);
 
-      await storage.write(key: userKey, value: jsonEncode(data['user']));
-      await storage.write(key: accessTokenKey, value: data['access_token']);
-      await storage.write(key: refreshTokenKey, value: data['refresh_token']);
-
-      // Too lazy to change repositories return type, so i used TopSnackbar here without context
-      TopSnackbar.successSnackbar(message: response.data['message']);
-
-      return Right(UserModel.fromJson(data['user']));
-    } on DioException {
-      await googleSignIn.signOut();
-      return const Left(Failure());
-    } catch (e) {
-      return const Left(Failure());
+      return Right(result.token.message);
+    } on ServerException catch (se) {
+      return Left(ServerFailure(
+        message: se.message,
+        statusCode: se.statusCode,
+      ));
+    } on CacheException catch (ce) {
+      return Left(CacheFailure(
+        message: ce.message,
+        statusCode: ce.statusCode,
+      ));
+    } on InternalException catch (ie) {
+      return Left(Failure(
+        message: ie.message,
+        statusCode: ie.statusCode,
+      ));
     }
   }
 
   @override
   Future<Either<Failure, String>> signOut() async {
     try {
-      final response = await authRemoteDataSource.signOut();
+      final result = await authRemoteDataSources.signOut();
+      await authLocalDataSources.clearCache();
 
-      await googleSignIn.signOut();
-      await storage.deleteAll();
-
-      return Right(response.data['message']);
-    } catch (e) {
-      return const Left(Failure());
+      return Right(result);
+    } on ServerException catch (se) {
+      return Left(ServerFailure(
+        message: se.message,
+        statusCode: se.statusCode,
+      ));
+    } on CacheException catch (ce) {
+      return Left(CacheFailure(
+        message: ce.message,
+        statusCode: ce.statusCode,
+      ));
+    } on InternalException catch (ie) {
+      return Left(Failure(
+        message: ie.message,
+        statusCode: ie.statusCode,
+      ));
     }
   }
 
   @override
   Future<Either<Failure, String>> updateProfile({required String name}) async {
     try {
-      final response = await authRemoteDataSource.updateProfile(name: name);
+      final result = await authRemoteDataSources.updateProfile(name: name);
 
-      return Right(response.data['message']);
-    } on DioException catch (de) {
-      switch (de.response?.statusCode) {
-        default:
-          return const Left(Failure());
-      }
-    } catch (e) {
-      return const Left(Failure());
+      return Right(result);
+    } on ServerException catch (se) {
+      return Left(ServerFailure(
+        message: se.message,
+        statusCode: se.statusCode,
+      ));
+    } on CacheException catch (ce) {
+      return Left(CacheFailure(
+        message: ce.message,
+        statusCode: ce.statusCode,
+      ));
+    } on InternalException catch (ie) {
+      return Left(Failure(
+        message: ie.message,
+        statusCode: ie.statusCode,
+      ));
     }
   }
 }
