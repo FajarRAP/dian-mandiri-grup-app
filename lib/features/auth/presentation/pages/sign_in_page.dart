229f5a377f0cb58b3f365e5e17cd4ae85701aa0e
Fajar Riansyah <fajary781@gmail.com>
2025-09-05 00:08:27 +0700

    refactor (sign in page): add success snackbar when sign in success

--- lib/features/auth/presentation/pages/sign_in_page.dart
+++ lib/features/auth/presentation/pages/sign_in_page.dart
@@ -26,7 +26,7 @@ class SignInPage extends StatelessWidget {
             child: BlocConsumer<AuthCubit, AuthState>(
               listener: (context, state) {
                 if (state is SignInLoaded) {
-                  // Already called Topsnackbar in repositories
+                  TopSnackbar.successSnackbar(message: state.message);
                   context.go(homeRoute);
                 }
 
