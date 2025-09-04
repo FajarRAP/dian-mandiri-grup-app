9ee179139545c6cfbb48d6592e71e03108213134
Fajar Riansyah <fajary781@gmail.com>
2025-09-05 00:05:56 +0700

    feat (auth remote data sources): improve separation and initial test

--- lib/features/auth/data/datasources/auth_remote_data_sources.dart
+++ lib/features/auth/data/datasources/auth_remote_data_sources.dart
@@ -1,48 +1,106 @@
 import 'package:dio/dio.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-import '../../../../core/common/constants.dart';
+import '../../../../core/exceptions/internal_exception.dart';
+import '../../../../core/helpers/helpers.dart';
+import '../models/sign_in_response_model.dart';
+import '../models/token_model.dart';
+import '../models/user_model.dart';
 
-abstract class AuthRemoteDataSources<T> {
-  Future<T> signIn({required String googleAccessToken});
-  Future<T> signOut();
-  Future<T> fetchUser();
-  Future<T> refreshToken({required String refreshToken});
-  Future<T> updateProfile({required String name});
+abstract class AuthRemoteDataSources {
+  Future<UserModel> fetchUser();
+  Future<TokenModel> refreshToken({required String refreshToken});
+  Future<SignInResponseModel> signIn();
+  Future<String> signOut();
+  Future<String> updateProfile({required String name});
 }
 
-class AuthRemoteDataSourcesImpl implements AuthRemoteDataSources<Response> {
-  const AuthRemoteDataSourcesImpl({required this.dio});
+class AuthRemoteDataSourcesImpl implements AuthRemoteDataSources {
+  const AuthRemoteDataSourcesImpl({
+    required this.dio,
+    required this.googleSignIn,
+  });
 
   final Dio dio;
+  final GoogleSignIn googleSignIn;
 
   @override
-  Future<Response> fetchUser() async {
-    return await dio.get('$authEndpoint/me');
+  Future<UserModel> fetchUser() async {
+    try {
+      final response = await dio.get('v1/auth/me');
+
+      return UserModel.fromJson(response.data['data']);
+    } on DioException catch (de) {
+      throw handleDioException(de);
+    } catch (e) {
+      throw InternalException(message: '$e');
+    }
   }
 
   @override
-  Future<Response> refreshToken({required String refreshToken}) async {
-    return await dio.post(
-      '$authEndpoint/refresh',
-      data: {'refresh_token': refreshToken},
-    );
+  Future<TokenModel> refreshToken({required String refreshToken}) async {
+    try {
+      final response = await dio.post(
+        'v1/auth/refresh',
+        data: {'refresh_token': refreshToken},
+      );
+
+      return TokenModel.fromJson(response.data);
+    } on DioException catch (de) {
+      throw handleDioException(de);
+    } catch (e) {
+      throw InternalException(message: '$e');
+    }
   }
 
   @override
-  Future<Response> signIn({required String googleAccessToken}) async {
-    return await dio.post(
-      '$authEndpoint/google',
-      data: {'access_token': googleAccessToken},
-    );
+  Future<SignInResponseModel> signIn() async {
+    try {
+      final googleSignInAccount = await googleSignIn.signIn();
+      final googleSignInAuthentication =
+          await googleSignInAccount?.authentication;
+      final googleAccessToken = googleSignInAuthentication?.accessToken;
+
+      final response = await dio.post(
+        'v1/auth/google',
+        data: {'access_token': googleAccessToken},
+      );
+
+      return SignInResponseModel.fromJson(response.data);
+    } on DioException catch (de) {
+      throw handleDioException(de);
+    } catch (e) {
+      throw InternalException(message: '$e');
+    }
   }
 
   @override
-  Future<Response> signOut() async {
-    return await dio.post('$authEndpoint/logout');
+  Future<String> signOut() async {
+    try {
+      final response = await dio.post('v1/auth/logout');
+      await googleSignIn.signOut();
+
+      return response.data['message'];
+    } on DioException catch (de) {
+      throw handleDioException(de);
+    } catch (e) {
+      throw InternalException(message: '$e');
+    }
   }
 
   @override
-  Future<Response> updateProfile({required String name}) async {
-    return await dio.put('$authEndpoint/profile', data: {'name': name});
+  Future<String> updateProfile({required String name}) async {
+    try {
+      final response = await dio.put(
+        'auth/profile',
+        data: {'name': name},
+      );
+
+      return response.data['message'];
+    } on DioException catch (de) {
+      throw handleDioException(de);
+    } catch (e) {
+      throw InternalException(message: '$e');
+    }
   }
 }
