bfc9d707a504f9225e6012bd4439796333655f9e
Fajar Riansyah <fajary781@gmail.com>
2025-09-21 16:18:57 +0700

    feat (helpers): add extension for DateTime and num

--- lib/core/helpers/helpers.dart
+++ lib/core/helpers/helpers.dart
@@ -7,6 +7,19 @@ import '../common/constants.dart';
 import '../exceptions/server_exception.dart';
 import '../failure/failure.dart';
 
+extension DateTimeFormatter on DateTime {
+  String get toYMD => DateFormat('y-MM-dd', 'id_ID').format(this);
+  String get toDMY => DateFormat('dd-MM-y HH:mm:ss', 'id_ID').format(this);
+  String get toDMYHMS => DateFormat('dd-MM-y', 'id_ID').format(this);
+  String get toHMS => DateFormat('HH:mm:ss', 'id_ID').format(this);
+}
+
+extension NumberFormatter on num {
+  String get toIDRCurrency =>
+      NumberFormat.currency(locale: 'id_ID', symbol: 'Rp ', decimalDigits: 0)
+          .format(this);
+}
+
 final dateFormat = DateFormat('y-MM-dd', 'id_ID');
 final dateTimeFormat = DateFormat('dd-MM-y HH:mm:ss', 'id_ID');
 final dMyFormat = DateFormat('dd-MM-y', 'id_ID');
