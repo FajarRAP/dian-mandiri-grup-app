b6070227d3c89d49dcad64622b4b531097aeb671
Fajar Riansyah <fajary781@gmail.com>
2025-11-22 20:20:42 +0700

    refactor (server exception): add error handling parameter to ServerException for improved error management

--- lib/core/exceptions/server_exception.dart
+++ lib/core/exceptions/server_exception.dart
@@ -1,8 +1,12 @@
 class ServerException implements Exception {
-  const ServerException({String? message, int? statusCode})
-      : message = message ?? 'Terjadi Kesalahan Server',
+  const ServerException({
+    String? message,
+    int? statusCode,
+    this.errors,
+  })  : message = message ?? 'Terjadi Kesalahan Server',
         statusCode = statusCode ?? 500;
 
   final String message;
   final int statusCode;
+  final Map<String, dynamic>? errors;
 }
