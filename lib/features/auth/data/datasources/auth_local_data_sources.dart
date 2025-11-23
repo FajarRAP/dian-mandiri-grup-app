3af84c4963c80fe6fc5c065f017a46bb3e54e772
Fajar Riansyah <fajary781@gmail.com>
2025-11-23 13:30:57 +0700

    refactor (auth local data source): simplify implementation

--- lib/features/auth/data/datasources/auth_local_data_sources.dart
+++ lib/features/auth/data/datasources/auth_local_data_sources.dart
@@ -3,11 +3,11 @@ import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../../../../core/common/constants.dart';
-import '../../../../core/exceptions/cache_exception.dart';
+import '../../../../core/utils/local_data_source_handler_mixin.dart';
 import '../models/token_model.dart';
 import '../models/user_model.dart';
 
-abstract class AuthLocalDataSources {
+abstract interface class AuthLocalDataSources {
   Future<void> cacheTokens({required TokenModel token});
   Future<void> cacheUser({required UserModel user});
   Future<void> clearCache();
@@ -16,67 +16,57 @@ abstract class AuthLocalDataSources {
   Future<UserModel?> getUser();
 }
 
-class AuthLocalDataSourcesImpl extends AuthLocalDataSources {
-  AuthLocalDataSourcesImpl({required this.storage});
+class AuthLocalDataSourcesImpl
+    with LocalDataSourceHandlerMixin
+    implements AuthLocalDataSources {
+  const AuthLocalDataSourcesImpl({required this.storage});
 
   final FlutterSecureStorage storage;
 
   @override
   Future<void> cacheTokens({required TokenModel token}) async {
-    try {
+    return await handleLocalDataSourceRequest<void>(() async {
       await storage.write(key: accessTokenKey, value: token.accessToken);
       await storage.write(key: refreshTokenKey, value: token.refreshToken);
-    } catch (e) {
-      throw CacheException(message: '$e');
-    }
+    });
   }
 
   @override
   Future<void> cacheUser({required UserModel user}) async {
-    try {
+    return await handleLocalDataSourceRequest<void>(() async {
       await storage.write(key: userKey, value: jsonEncode(user.toJson()));
-    } catch (e) {
-      throw CacheException(message: '$e');
-    }
+    });
   }
 
   @override
   Future<void> clearCache() async {
-    try {
+    return await handleLocalDataSourceRequest<void>(() async {
       await storage.deleteAll();
-    } catch (e) {
-      throw CacheException(message: '$e');
-    }
+    });
   }
 
   @override
   Future<String?> getAccessToken() async {
-    try {
+    return await handleLocalDataSourceRequest<String?>(() async {
       return await storage.read(key: accessTokenKey);
-    } catch (e) {
-      throw CacheException(message: '$e');
-    }
+    });
   }
 
   @override
   Future<String?> getRefreshToken() async {
-    try {
+    return await handleLocalDataSourceRequest<String?>(() async {
       return await storage.read(key: refreshTokenKey);
-    } catch (e) {
-      throw CacheException(message: '$e');
-    }
+    });
   }
 
   @override
   Future<UserModel?> getUser() async {
-    try {
+    return await handleLocalDataSourceRequest<UserModel?>(() async {
       final user = await storage.read(key: userKey);
 
       if (user == null) return null;
 
       return UserModel.fromJson(jsonDecode(user));
-    } catch (e) {
-      throw CacheException(message: '$e');
-    }
+    });
   }
 }
