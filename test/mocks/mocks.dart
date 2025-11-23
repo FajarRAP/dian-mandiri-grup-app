6430c884063815d73a0a161e47e33d1f3517ce07
Fajar Riansyah <fajary781@gmail.com>
2025-11-23 23:37:35 +0700

    feat (test): add mocks

---
+++ test/mocks/mocks.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:ship_tracker/core/services/google_sign_in_service.dart';
+import 'package:ship_tracker/features/auth/data/datasources/auth_local_data_source.dart';
+import 'package:ship_tracker/features/auth/data/datasources/auth_remote_data_source.dart';
+
+class MockDio extends Mock implements Dio {}
+
+class MockStorage extends Mock implements FlutterSecureStorage {}
+
+class MockGoogleSignIn extends Mock implements GoogleSignIn {}
+
+class MockGoogleSignInService extends Mock implements GoogleSignInService {}
+
+class MockAuthLocalDataSource extends Mock implements AuthLocalDataSource {}
+
+class MockAuthRemoteDataSource extends Mock implements AuthRemoteDataSource {}
