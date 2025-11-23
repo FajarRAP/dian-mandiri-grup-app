4bcd01d15740091300cc68c4e97b881f51c8c2fc
Fajar Riansyah <fajary781@gmail.com>
2025-11-23 13:01:47 +0700

    feat (core): add typedefs

---
+++ lib/core/utils/typedefs.dart
@@ -0,0 +1,13 @@
+typedef JsonMap = Map<String, dynamic>;
+
+typedef ListJsonMap = List<Map<String, dynamic>>;
+
+extension JsonParser on JsonMap {
+  DateTime? toNullableDateTime(String key) {
+    if (this[key] is String) {
+      return DateTime.tryParse(this[key]);
+    }
+
+    return null;
+  }
+}
