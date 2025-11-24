7d8650bc1f3d81822623b86b68ac67edaf31068a
Fajar Riansyah <fajary781@gmail.com>
2025-11-24 19:19:38 +0700

    feat (supplier remote data source): add unit tests and use DioHandlerMixin

--- lib/features/supplier/data/datasources/supplier_remote_data_source.dart
+++ lib/features/supplier/data/datasources/supplier_remote_data_source.dart
@@ -1,48 +1,50 @@
 import 'package:dio/dio.dart';
 
 import '../../../../core/common/dropdown_entity.dart';
-import '../../../../core/exceptions/internal_exception.dart';
-import '../../../../core/helpers/helpers.dart';
+import '../../../../core/network/dio_handler_mixin.dart';
+import '../../../../core/utils/extensions.dart';
+import '../../../../core/utils/typedefs.dart';
 import '../../domain/entities/supplier_detail_entity.dart';
+import '../../domain/entities/supplier_entity.dart';
+import '../../domain/usecases/fetch_supplier_use_case.dart';
 import '../../domain/usecases/fetch_suppliers_dropdown_use_case.dart';
 import '../../domain/usecases/fetch_suppliers_use_case.dart';
 import '../../domain/usecases/insert_supplier_use_case.dart';
+import '../../domain/usecases/update_supplier_use_case.dart';
 import '../models/supplier_detail_model.dart';
 import '../models/supplier_model.dart';
 
-abstract class SupplierRemoteDataSource {
-  Future<SupplierDetailModel> fetchSupplier({required String supplierId});
-  Future<List<SupplierModel>> fetchSuppliers(
-      {required FetchSuppliersUseCaseParams params});
+abstract interface class SupplierRemoteDataSource {
+  Future<SupplierDetailEntity> fetchSupplier(FetchSupplierUseCaseParams params);
+  Future<List<SupplierEntity>> fetchSuppliers(
+      FetchSuppliersUseCaseParams params);
   Future<List<DropdownEntity>> fetchSuppliersDropdown(
-      {required FetchSuppliersDropdownUseCaseParams params});
-  Future<String> insertSupplier({required InsertSupplierUseCaseParams params});
-  Future<String> updateSupplier({required SupplierDetailEntity params});
+      FetchSuppliersDropdownUseCaseParams params);
+  Future<String> insertSupplier(InsertSupplierUseCaseParams params);
+  Future<String> updateSupplier(UpdateSupplierUseCaseParams params);
 }
 
-class SupplierRemoteDataSourceImpl implements SupplierRemoteDataSource {
-  SupplierRemoteDataSourceImpl({required this.dio});
+class SupplierRemoteDataSourceImpl
+    with DioHandlerMixin
+    implements SupplierRemoteDataSource {
+  const SupplierRemoteDataSourceImpl({required this.dio});
 
   final Dio dio;
 
   @override
-  Future<SupplierDetailModel> fetchSupplier(
-      {required String supplierId}) async {
-    try {
-      final response = await dio.get('v1/supplier/$supplierId');
+  Future<SupplierDetailEntity> fetchSupplier(
+      FetchSupplierUseCaseParams params) async {
+    return handleDioRequest<SupplierDetailEntity>(() async {
+      final response = await dio.get('v1/supplier/${params.supplierId}');
 
-      return SupplierDetailModel.fromJson(response.data['data']);
-    } on DioException catch (de) {
-      throw handleDioException(de);
-    } catch (e) {
-      throw InternalException(message: '$e');
-    }
+      return SupplierDetailModel.fromJson(response.data['data']).toEntity();
+    });
   }
 
   @override
-  Future<List<SupplierModel>> fetchSuppliers(
-      {required FetchSuppliersUseCaseParams params}) async {
-    try {
+  Future<List<SupplierEntity>> fetchSuppliers(
+      FetchSuppliersUseCaseParams params) async {
+    return handleDioRequest<List<SupplierEntity>>(() async {
       final response = await dio.get(
         'v1/supplier',
         queryParameters: {
@@ -53,21 +55,16 @@ class SupplierRemoteDataSourceImpl implements SupplierRemoteDataSource {
           'page': params.page,
         },
       );
-      final contents =
-          List<Map<String, dynamic>>.from(response.data['data']['content']);
+      final contents = List<JsonMap>.from(response.data['data']['content']);
 
-      return contents.map(SupplierModel.fromJson).toList();
-    } on DioException catch (de) {
-      throw handleDioException(de);
-    } catch (e) {
-      throw InternalException(message: '$e');
-    }
+      return contents.map((e) => SupplierModel.fromJson(e).toEntity()).toList();
+    });
   }
 
   @override
   Future<List<DropdownEntity>> fetchSuppliersDropdown(
-      {required FetchSuppliersDropdownUseCaseParams params}) async {
-    try {
+      FetchSuppliersDropdownUseCaseParams params) async {
+    return handleDioRequest<List<DropdownEntity>>(() async {
       final response = await dio.get(
         'v1/supplier/dropdown',
         queryParameters: {
@@ -76,28 +73,20 @@ class SupplierRemoteDataSourceImpl implements SupplierRemoteDataSource {
           'page': params.page,
         },
       );
-      final contents =
-          List<Map<String, dynamic>>.from(response.data['data']['content']);
+      final contents = List<JsonMap>.from(response.data['data']['content']);
 
       return contents.map(DropdownEntity.fromJson).toList();
-    } on DioException catch (de) {
-      throw handleDioException(de);
-    } catch (e) {
-      throw InternalException(message: '$e');
-    }
+    });
   }
 
   @override
-  Future<String> insertSupplier(
-      {required InsertSupplierUseCaseParams params}) async {
-    try {
+  Future<String> insertSupplier(InsertSupplierUseCaseParams params) async {
+    return handleDioRequest<String>(() async {
       final response = await dio.post(
         'v1/supplier',
         data: FormData.fromMap({
           'address': params.address,
-          'avatar': params.avatar != null
-              ? await MultipartFile.fromFile(params.avatar!)
-              : null,
+          'avatar': await params.avatar.toMultipartFile(),
           'email': params.email,
           'name': params.name,
           'phoneNumber': params.phoneNumber,
@@ -105,35 +94,27 @@ class SupplierRemoteDataSourceImpl implements SupplierRemoteDataSource {
       );
 
       return response.data['message'];
-    } on DioException catch (de) {
-      throw handleDioException(de);
-    } catch (e) {
-      throw InternalException(message: '$e');
-    }
+    });
   }
 
   @override
-  Future<String> updateSupplier({required SupplierDetailEntity params}) async {
-    final supplierDetail = SupplierDetailModel.fromEntity(params);
-    final payload = supplierDetail.toJson();
-    final isAvatarNull = supplierDetail.avatarUrl != null;
-    if (isAvatarNull &&
-        !(supplierDetail.avatarUrl?.startsWith('https://') ?? false)) {
-      payload['avatar'] =
-          await MultipartFile.fromFile(supplierDetail.avatarUrl!);
-    }
+  Future<String> updateSupplier(UpdateSupplierUseCaseParams params) async {
+    return handleDioRequest<String>(() async {
+      final supplierDetail =
+          SupplierDetailModel.fromEntity(params.supplierDetailEntity);
+      final payload = supplierDetail.toJson();
+      final newAvatar = await supplierDetail.avatarUrl.toMultipartFile();
 
-    try {
+      if (newAvatar != null) {
+        payload['avatar'] = newAvatar;
+      }
+
       final response = await dio.put(
-        'v1/supplier/${params.id}',
+        'v1/supplier/${params.supplierDetailEntity.id}',
         data: FormData.fromMap(payload),
       );
 
       return response.data['message'];
-    } on DioException catch (de) {
-      throw handleDioException(de);
-    } catch (e) {
-      throw InternalException(message: '$e');
-    }
+    });
   }
 }
---
+++ test/features/supplier/data/datasources/supplier_remote_data_source_test.dart
@@ -0,0 +1,251 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:ship_tracker/core/errors/exceptions.dart';
+import 'package:ship_tracker/features/supplier/data/datasources/supplier_remote_data_source.dart';
+
+import '../../../../core/utils/fixture_reader.dart';
+import '../../../../helpers/testdata/supplier_test_data.dart';
+import '../../../../helpers/testdata/test_data.dart';
+import '../../../../mocks/mocks.dart';
+
+void main() {
+  const fixtureReader = FixtureReader(domain: 'supplier');
+  late MockDio mockDio;
+  late SupplierRemoteDataSourceImpl supplierRemoteDataSource;
+
+  setUp(() {
+    mockDio = MockDio();
+    supplierRemoteDataSource = SupplierRemoteDataSourceImpl(dio: mockDio);
+  });
+
+  group('fetch supplier remote data source test', () {
+    const params = tFetchSupplierParams;
+    const resultMatcher = tFetchSupplierSuccess;
+
+    test('should return SupplierDetailEntity when request is successful',
+        () async {
+      // arrange
+      final jsonString = fixtureReader.dataSource('fetch_supplier.json');
+      final json = jsonDecode(jsonString);
+      when(() => mockDio.get(any())).thenAnswer((_) async => Response(
+          requestOptions: RequestOptions(), data: json, statusCode: 200));
+
+      // act
+      final result = await supplierRemoteDataSource.fetchSupplier(params);
+
+      // assert
+      expect(result, resultMatcher);
+      verify(() => mockDio.get('v1/supplier/${params.supplierId}')).called(1);
+    });
+
+    test('should throw ServerException when API returns 4xx/5xx', () async {
+      // arrange
+      when(() => mockDio.get(any())).thenThrow(DioException(
+          requestOptions: RequestOptions(),
+          error: tServerException,
+          type: DioExceptionType.badResponse));
+
+      // act
+      final future = supplierRemoteDataSource.fetchSupplier(params);
+
+      // assert
+      await expectLater(() => future, throwsA(isA<ServerException>()));
+      verify(() => mockDio.get('v1/supplier/${params.supplierId}')).called(1);
+    });
+  });
+
+  group('fetch suppliers remote data source test', () {
+    const params = tFetchSuppliersParams;
+    const resultMatcher = tFetchSuppliersSuccess;
+
+    test('should return List<SupplierEntity> when request is successful',
+        () async {
+      // arrange
+      final jsonString = fixtureReader.dataSource('fetch_suppliers.json');
+      final json = jsonDecode(jsonString);
+      when(() => mockDio.get(any(),
+              queryParameters: any(named: 'queryParameters')))
+          .thenAnswer((_) async => Response(
+              requestOptions: RequestOptions(), data: json, statusCode: 200));
+
+      // act
+      final result = await supplierRemoteDataSource.fetchSuppliers(params);
+
+      // assert
+      expect(result, resultMatcher);
+      verify(() => mockDio.get(
+            'v1/supplier',
+            queryParameters: {
+              'column': params.column,
+              'order': params.sort,
+              'search': params.search,
+              'limit': params.limit,
+              'page': params.page,
+            },
+          )).called(1);
+    });
+
+    test('should throw ServerException when API returns 4xx/5xx', () async {
+      // arrange
+      when(() => mockDio.get(any(),
+              queryParameters: any(named: 'queryParameters')))
+          .thenThrow(DioException(
+              requestOptions: RequestOptions(),
+              error: tServerException,
+              type: DioExceptionType.badResponse));
+
+      // act
+      final future = supplierRemoteDataSource.fetchSuppliers(params);
+
+      // assert
+      await expectLater(() => future, throwsA(isA<ServerException>()));
+      verify(() => mockDio.get(
+            'v1/supplier',
+            queryParameters: {
+              'column': params.column,
+              'order': params.sort,
+              'search': params.search,
+              'limit': params.limit,
+              'page': params.page,
+            },
+          )).called(1);
+    });
+  });
+
+  group('fetch suppliers dropdown remote data source test', () {
+    const params = tFetchSuppliersDropdownParams;
+    const resultMatcher = tFetchSuppliersDropdownSuccess;
+
+    test('should return List<DropdownEntity> when request is successful',
+        () async {
+      // arrange
+      final jsonString =
+          fixtureReader.dataSource('fetch_suppliers_dropdown.json');
+      final json = jsonDecode(jsonString);
+      when(() => mockDio.get(any(),
+              queryParameters: any(named: 'queryParameters')))
+          .thenAnswer((_) async => Response(
+              requestOptions: RequestOptions(), data: json, statusCode: 200));
+
+      // act
+      final result =
+          await supplierRemoteDataSource.fetchSuppliersDropdown(params);
+
+      // assert
+      expect(result, resultMatcher);
+      verify(() => mockDio.get(
+            'v1/supplier/dropdown',
+            queryParameters: {
+              'search': params.search,
+              'limit': params.limit,
+              'page': params.page,
+            },
+          )).called(1);
+    });
+
+    test('should throw ServerException when API returns 4xx/5xx', () async {
+      // arrange
+      when(() => mockDio.get(any(),
+              queryParameters: any(named: 'queryParameters')))
+          .thenThrow(DioException(
+              requestOptions: RequestOptions(),
+              error: tServerException,
+              type: DioExceptionType.badResponse));
+
+      // act
+      final future = supplierRemoteDataSource.fetchSuppliersDropdown(params);
+
+      // assert
+      await expectLater(() => future, throwsA(isA<ServerException>()));
+      verify(() => mockDio.get(
+            'v1/supplier/dropdown',
+            queryParameters: {
+              'search': params.search,
+              'limit': params.limit,
+              'page': params.page,
+            },
+          )).called(1);
+    });
+  });
+
+  group('create supplier remote data source test', () {
+    const params = tCreateSupplierParams;
+    const resultMatcher = tCreateSupplierSuccess;
+
+    test('should return String message when request is successful', () async {
+      // arrange
+      final jsonString = fixtureReader.dataSource('create_supplier.json');
+      final json = jsonDecode(jsonString);
+      when(() => mockDio.post(any(), data: any(named: 'data'))).thenAnswer(
+          (_) async => Response(
+              requestOptions: RequestOptions(), data: json, statusCode: 200));
+
+      // act
+      final result = await supplierRemoteDataSource.insertSupplier(params);
+
+      // assert
+      expect(result, resultMatcher);
+      verify(() => mockDio.post('v1/supplier', data: any(named: 'data')))
+          .called(1);
+    });
+
+    test('should throw ServerException when API returns 4xx/5xx', () async {
+      // arrange
+      when(() => mockDio.post(any(), data: any(named: 'data'))).thenThrow(
+          DioException(
+              requestOptions: RequestOptions(),
+              error: tServerException,
+              type: DioExceptionType.badResponse));
+
+      // act
+      final future = supplierRemoteDataSource.insertSupplier(params);
+
+      // assert
+      await expectLater(() => future, throwsA(isA<ServerException>()));
+      verify(() => mockDio.post('v1/supplier', data: any(named: 'data')))
+          .called(1);
+    });
+  });
+
+  group('update supplier remote data source test', () {
+    const params = tUpdateSupplierParams;
+    const resultMatcher = tUpdateSupplierSuccess;
+
+    test('should return String message when request is successful', () async {
+      // arrange
+      final jsonString = fixtureReader.dataSource('update_supplier.json');
+      final json = jsonDecode(jsonString);
+      when(() => mockDio.put(any(), data: any(named: 'data'))).thenAnswer(
+          (_) async => Response(
+              requestOptions: RequestOptions(), data: json, statusCode: 200));
+
+      // act
+      final result = await supplierRemoteDataSource.updateSupplier(params);
+
+      // assert
+      expect(result, resultMatcher);
+      verify(() => mockDio.put('v1/supplier/${params.supplierDetailEntity.id}',
+          data: any(named: 'data'))).called(1);
+    });
+
+    test('should throw ServerException when API returns 4xx/5xx', () async {
+      // arrange
+      when(() => mockDio.put(any(), data: any(named: 'data'))).thenThrow(
+          DioException(
+              requestOptions: RequestOptions(),
+              error: tServerException,
+              type: DioExceptionType.badResponse));
+
+      // act
+      final future = supplierRemoteDataSource.updateSupplier(params);
+
+      // assert
+      await expectLater(() => future, throwsA(isA<ServerException>()));
+      verify(() => mockDio.put('v1/supplier/${params.supplierDetailEntity.id}',
+          data: any(named: 'data'))).called(1);
+    });
+  });
+}
