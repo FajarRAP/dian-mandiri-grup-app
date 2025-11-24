25a8f22b2d0614a15ff5ccfd1d5ac2a504e186af
Fajar Riansyah <fajary781@gmail.com>
2025-11-24 15:00:19 +0700

    feat (mocks): add MockShipmentRemoteDataSource for testing

--- test/mocks/mocks.dart
+++ test/mocks/mocks.dart
@@ -5,6 +5,7 @@ import 'package:mocktail/mocktail.dart';
 import 'package:ship_tracker/core/services/google_sign_in_service.dart';
 import 'package:ship_tracker/features/auth/data/datasources/auth_local_data_source.dart';
 import 'package:ship_tracker/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:ship_tracker/features/tracker/data/datasources/shipment_remote_data_source.dart';
 
 class MockDio extends Mock implements Dio {}
 
@@ -17,3 +18,6 @@ class MockGoogleSignInService extends Mock implements GoogleSignInService {}
 class MockAuthLocalDataSource extends Mock implements AuthLocalDataSource {}
 
 class MockAuthRemoteDataSource extends Mock implements AuthRemoteDataSource {}
+
+class MockShipmentRemoteDataSource extends Mock
+    implements ShipmentRemoteDataSource {}
