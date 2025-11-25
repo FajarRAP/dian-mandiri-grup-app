eb7da9b3e85f9d90df97b039f4e80acd212d5429
Fajar Riansyah <fajary781@gmail.com>
2025-11-25 14:41:46 +0700

    feat (core): add splash page

---
+++ lib/core/presentation/pages/splash_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../common/constants/app_images.dart';
+import '../../router/route_names.dart';
+import '../cubit/app_cubit.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AppCubit, AppState>(
+      bloc: context.read<AppCubit>()..initializeApp(),
+      listener: (context, state) {
+        if (state is AppSuccess) {
+          context.goNamed(Routes.home);
+        }
+
+        if (state is NavigateToLogin) {
+          context.goNamed(Routes.login);
+        }
+      },
+      child: Scaffold(
+        body: Center(
+          child: Container(
+            alignment: .center,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.appIcon),
+                fit: .contain,
+              ),
+              shape: .circle,
+            ),
+            width: 200,
+          ),
+        ),
+      ),
+    );
+  }
+}
