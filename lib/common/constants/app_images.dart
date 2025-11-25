4448e3ae8aaf7d8da84818cfef4261d0b20d4aac
Fajar Riansyah <fajary781@gmail.com>
2025-11-25 20:40:53 +0700

    refactor (app images): remove redundant 'Icon' suffix from image constants

--- lib/common/constants/app_images.dart
+++ lib/common/constants/app_images.dart
@@ -1,17 +1,17 @@
 class AppImages {
   const AppImages._();
 
-  static const spreadsheetIcon = 'assets/icons/excel.png';
-  static const scanReceiptIcon = 'assets/icons/scan.png';
-  static const pickUpReceiptIcon = 'assets/icons/pick-up.png';
-  static const checkReceiptIcon = 'assets/icons/check.png';
-  static const packReceiptIcon = 'assets/icons/pack.png';
-  static const sendReceiptIcon = 'assets/icons/send.png';
-  static const returnReceiptIcon = 'assets/icons/return.png';
-  static const cancelReceiptIcon = 'assets/icons/cancel.png';
-  static const reportReceiptIcon = 'assets/icons/report.png';
-  static const checkReceiptStatusIcon = 'assets/icons/check.png';
-  static const googleIcon = 'assets/icons/google.png';
-  static const personIcon = 'assets/icons/person.png';
-  static const appIcon = 'assets/icons/app-icon.png';
+  static const spreadsheet = 'assets/icons/excel.png';
+  static const scanReceipt = 'assets/icons/scan.png';
+  static const pickUpReceipt = 'assets/icons/pick-up.png';
+  static const checkReceipt = 'assets/icons/check.png';
+  static const packReceipt = 'assets/icons/pack.png';
+  static const sendReceipt = 'assets/icons/send.png';
+  static const returnReceipt = 'assets/icons/return.png';
+  static const cancelReceipt = 'assets/icons/cancel.png';
+  static const reportReceipt = 'assets/icons/report.png';
+  static const checkReceiptStatus = 'assets/icons/check.png';
+  static const google = 'assets/icons/google.png';
+  static const person = 'assets/icons/person.png';
+  static const app = 'assets/icons/app-icon.png';
 }
