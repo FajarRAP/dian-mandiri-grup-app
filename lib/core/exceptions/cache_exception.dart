1064955a7aa5a64781fa8835d2ac87536433bbfa
Fajar Riansyah <fajary781@gmail.com>
2025-11-25 11:49:48 +0700

    chore: remove unused files

--- lib/core/exceptions/cache_exception.dart
+++
@@ -1,8 +0,0 @@
-class CacheException implements Exception {
-  const CacheException({String? message, int? statusCode})
-      : message = message ?? 'Terjadi Kesalahan Cache',
-        statusCode = statusCode ?? 500;
-
-  final String message;
-  final int statusCode;
-}
