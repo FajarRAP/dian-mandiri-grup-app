64eeb4baaecd5856948d82031a1a13ee57f82e69
Fajar Riansyah <fajary781@gmail.com>
2025-11-25 15:28:36 +0700

    refactor (auth): update sign-in flow to return SignInEntity and improve type safety

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,18 +1,21 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
+import '../entities/sign_in_entity.dart';
 import '../entities/user_entity.dart';
 import '../usecases/refresh_token_use_case.dart';
 import '../usecases/update_profile_use_case.dart';
 
 abstract interface class AuthRepository {
-  Future<Either<Failure, String>> signIn();
+  Future<Either<Failure, SignInEntity>> signIn();
   Future<Either<Failure, String>> signOut();
   Future<Either<Failure, UserEntity>> fetchUser();
   Future<Either<Failure, String>> refreshToken(
-      RefreshTokenUseCaseParams params);
+    RefreshTokenUseCaseParams params,
+  );
   Future<Either<Failure, UserEntity?>> fetchUserFromStorage();
   Future<Either<Failure, String>> updateProfile(
-      UpdateProfileUseCaseParams params);
+    UpdateProfileUseCaseParams params,
+  );
   Future<Either<Failure, String?>> getRefreshToken();
 }
--- lib/features/auth/domain/usecases/sign_in_use_case.dart
+++ lib/features/auth/domain/usecases/sign_in_use_case.dart
@@ -2,15 +2,16 @@ import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
 import '../../../../core/usecase/use_case.dart';
+import '../entities/sign_in_entity.dart';
 import '../repositories/auth_repository.dart';
 
-class SignInUseCase implements UseCase<String, NoParams> {
+class SignInUseCase implements UseCase<SignInEntity, NoParams> {
   const SignInUseCase({required this.authRepository});
 
   final AuthRepository authRepository;
 
   @override
-  Future<Either<Failure, String>> execute(NoParams params) async {
+  Future<Either<Failure, SignInEntity>> execute(NoParams params) async {
     return await authRepository.signIn();
   }
 }
