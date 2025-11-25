3f16d6bb466d3c41f5cfab65f748ddf5639095bf
Fajar Riansyah <fajary781@gmail.com>
2025-11-25 20:41:09 +0700

    feat (constants): add app permissions

---
+++ lib/common/constants/app_permissions.dart
@@ -0,0 +1,12 @@
+class AppPermissions {
+  const AppPermissions._();
+
+  static const superAdmin = 'super_admin';
+  static const scanShipment = 'shipment:scan';
+  static const pickUpShipment = 'shipment:pick_up';
+  static const checkShipment = 'shipment:check';
+  static const packShipment = 'shipment:pack';
+  static const sendShipment = 'shipment:send';
+  static const returnShipment = 'shipment:return';
+  static const cancelShipment = 'shipment:cancel';
+}
