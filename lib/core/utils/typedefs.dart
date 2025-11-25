bee9b28de998ddedb2bd2069fde74c1d93d0cb90
Fajar Riansyah <fajary781@gmail.com>
2025-11-25 11:36:52 +0700

    feat (typedefs): add ListJsonMapNullable typedef for better null handling

--- lib/core/utils/typedefs.dart
+++ lib/core/utils/typedefs.dart
@@ -2,6 +2,8 @@ typedef JsonMap = Map<String, dynamic>;
 
 typedef ListJsonMap = List<Map<String, dynamic>>;
 
+typedef ListJsonMapNullable = List<Map<String, dynamic>?>;
+
 extension JsonParser on JsonMap {
   DateTime? toNullableDateTime(String key) {
     if (this[key] is String) {
