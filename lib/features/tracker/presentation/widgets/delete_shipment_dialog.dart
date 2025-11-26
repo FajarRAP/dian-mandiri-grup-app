474a3d1690d8d9e26dd8435546df8f4e00f1615f
Fajar Riansyah <fajary781@gmail.com>
2025-11-26 17:35:02 +0700

    feat (tracker): add delete shipment dialog widget

---
+++ lib/features/tracker/presentation/widgets/delete_shipment_dialog.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../core/widgets/confirmation_dialog.dart';
+import '../cubit/shipment_list/shipment_list_cubit.dart';
+
+class DeleteShipmentDialog extends StatelessWidget {
+  const DeleteShipmentDialog({super.key, required this.shipmentId});
+
+  final String shipmentId;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<ShipmentListCubit, ShipmentListState>(
+      listener: (context, state) {
+        if (state.status == ShipmentListStatus.actionSuccess) {
+          context.pop();
+        }
+      },
+      builder: (context, state) {
+        final onAction = switch (state.status) {
+          ShipmentListStatus.actionInProgress => null,
+          _ => () => context.read<ShipmentListCubit>().deleteShipment(
+            shipmentId: shipmentId,
+          ),
+        };
+
+        return ConfirmationDialog(
+          onAction: onAction,
+          actionText: 'Hapus',
+          body: 'Yakin ingin menghapus resi ini?',
+          title: 'Hapus Resi',
+        );
+      },
+    );
+  }
+}
