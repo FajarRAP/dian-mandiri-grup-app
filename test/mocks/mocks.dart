8b5788d40023759acba94b8da4c90e4fa5ec2572
Fajar Riansyah <fajary781@gmail.com>
2025-11-28 16:27:50 +0700

    feat (mocks): add file service mock class

--- test/mocks/mocks.dart
+++ test/mocks/mocks.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:ship_tracker/core/services/file_service.dart';
 import 'package:ship_tracker/core/services/google_sign_in_service.dart';
 import 'package:ship_tracker/features/auth/data/datasources/auth_local_data_source.dart';
 import 'package:ship_tracker/features/auth/data/datasources/auth_remote_data_source.dart';
@@ -16,6 +17,8 @@ class MockStorage extends Mock implements FlutterSecureStorage {}
 class MockGoogleSignIn extends Mock implements GoogleSignIn {}
 
 class MockGoogleSignInService extends Mock implements GoogleSignInService {}
+
+class MockFileService extends Mock implements FileService {}
 
 class MockAuthLocalDataSource extends Mock implements AuthLocalDataSource {}
 
