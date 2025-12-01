ff9d066b33679b93afc3ee3fdfb1d887d38447a4
Fajar Riansyah <fajary781@gmail.com>
2025-12-01 12:36:30 +0700

    refactor (editable avatar): replace imageFile with imagePath and streamline avatar handling

--- lib/features/supplier/presentation/widgets/editable_avatar.dart
+++ lib/features/supplier/presentation/widgets/editable_avatar.dart
@@ -5,13 +5,21 @@ import 'package:flutter/material.dart';
 import '../../../../core/utils/extensions.dart';
 
 class EditableAvatar extends StatelessWidget {
-  const EditableAvatar({super.key, this.onTap, this.imageFile});
+  const EditableAvatar({super.key, this.onTap, this.imagePath});
 
   final void Function()? onTap;
-  final File? imageFile;
+  final String? imagePath;
 
   @override
   Widget build(BuildContext context) {
+    final hasAvatar = imagePath != null && imagePath!.isNotEmpty;
+    final onlineAvatar = imagePath?.startsWith('http') ?? false;
+    final ImageProvider? avatar = hasAvatar
+        ? (onlineAvatar
+              ? NetworkImage(imagePath!)
+              : FileImage(File(imagePath!)))
+        : null;
+
     return GestureDetector(
       onTap: onTap,
       child: UnconstrainedBox(
@@ -20,8 +28,8 @@ class EditableAvatar extends StatelessWidget {
             CircleAvatar(
               radius: 50,
               backgroundColor: Colors.grey.shade300,
-              foregroundImage: imageFile == null ? null : FileImage(imageFile!),
-              child: imageFile == null
+              foregroundImage: avatar,
+              child: !hasAvatar
                   ? Icon(
                       Icons.person_outline,
                       color: Colors.grey.shade400,
@@ -29,18 +37,27 @@ class EditableAvatar extends StatelessWidget {
                     )
                   : null,
             ),
-            Positioned(
-              right: 0,
-              bottom: 0,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: context.colorScheme.primary,
-                  shape: .circle,
+            if (onTap != null)
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: context.colorScheme.surface,
+                      width: 2,
+                    ),
+                    color: context.colorScheme.primary,
+                    shape: .circle,
+                  ),
+                  padding: const .all(4),
+                  child: Icon(
+                    Icons.edit,
+                    color: context.colorScheme.onPrimary,
+                    size: 16,
+                  ),
                 ),
-                padding: const .all(4),
-                child: Icon(Icons.edit, color: context.colorScheme.onPrimary),
               ),
-            ),
           ],
         ),
       ),
