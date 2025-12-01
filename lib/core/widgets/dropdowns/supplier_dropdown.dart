42fb6135b84ac0fdee528d14547a2ea6f7ddedd6
Fajar Riansyah <fajary781@gmail.com>
2025-12-01 14:25:30 +0700

    refactor (supplier dropdown): convert SupplierDropdown to a stateless widget and simplify state management

--- lib/core/widgets/dropdowns/supplier_dropdown.dart
+++ lib/core/widgets/dropdowns/supplier_dropdown.dart
@@ -1,106 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
 
-import '../../../features/supplier/presentation/cubit/supplier_cubit.dart';
+import '../../../service_container.dart';
 import '../../common/dropdown_entity.dart';
+import '../../presentation/cubit/dropdown_cubit.dart';
+import '../../presentation/widgets/loading_indicator.dart';
+import '../../presentation/widgets/pagination_listener.dart';
 import '../dropdown_modal_item.dart';
 import '../dropdown_search_modal.dart';
 
-class SupplierDropdown extends StatefulWidget {
-  const SupplierDropdown({
-    super.key,
-    required this.onTap,
-  });
+class SupplierDropdown extends StatelessWidget {
+  const SupplierDropdown({super.key, required this.onTap});
 
   final void Function(DropdownEntity supplier) onTap;
 
   @override
-  State<SupplierDropdown> createState() => _SupplierDropdownState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => getIt<DropdownCubit>()..fetchSuppliers(),
+      child: Padding(
+        padding: .only(bottom: MediaQuery.viewInsetsOf(context).bottom),
+        child: DropdownSearchModal(
+          search: (keyword) =>
+              context.read<DropdownCubit>().fetchSuppliers(query: keyword),
+          title: 'Supplier',
+          child: Expanded(
+            child: BlocBuilder<DropdownCubit, DropdownState>(
+              builder: (context, state) {
+                return switch (state.status) {
+                  .inProgress => const LoadingIndicator(),
+                  .success => _SuccessWidget(
+                    onTap: onTap,
+                    suppliers: state.items,
+                  ),
+                  _ => const SizedBox(),
+                };
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _SupplierDropdownState extends State<SupplierDropdown> {
-  late final SupplierCubit _supplierCubit;
-  String? _search;
+class _SuccessWidget extends StatelessWidget {
+  const _SuccessWidget({required this.onTap, required this.suppliers});
 
-  @override
-  void initState() {
-    super.initState();
-    _supplierCubit = context.read<SupplierCubit>()..fetchSuppliersDropdown();
-  }
+  final void Function(DropdownEntity supplier) onTap;
+  final List<DropdownEntity> suppliers;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        bottom: MediaQuery.viewInsetsOf(context).bottom,
-      ),
-      child: DropdownSearchModal(
-        search: (keyword) {
-          _search = keyword;
-          _supplierCubit.fetchSuppliersDropdown(search: _search);
-        },
-        title: 'Supplier',
-        child: BlocBuilder<SupplierCubit, SupplierState>(
-          bloc: _supplierCubit,
-          buildWhen: (previous, current) => current is FetchSuppliersDropdown,
-          builder: (context, state) {
-            if (state is FetchSuppliersDropdownLoading) {
-              return const Center(
-                child: CircularProgressIndicator.adaptive(),
-              );
-            }
-
-            if (state is FetchSuppliersDropdownLoaded) {
-              return Flexible(
-                child: NotificationListener<ScrollNotification>(
-                  onNotification: (scrollState) {
-                    if (scrollState.runtimeType == ScrollEndNotification &&
-                        _supplierCubit.state is! ListPaginateLast) {
-                      _supplierCubit.fetchSuppliersDropdownPaginate(
-                          search: _search);
-                    }
-
-                    return false;
-                  },
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListView.separated(
-                        itemBuilder: (context, index) => GestureDetector(
-                          onTap: () => widget.onTap(state.suppliers[index]),
-                          child: DropdownModalItem(
-                            child: Text(state.suppliers[index].value),
-                          ),
-                        ),
-                        separatorBuilder: (context, index) =>
-                            const SizedBox(height: 12),
-                        itemCount: state.suppliers.length,
-                        padding: const EdgeInsets.symmetric(vertical: 12),
-                        shrinkWrap: true,
-                      ),
-                      // Widget when Pagination
-                      BlocBuilder<SupplierCubit, SupplierState>(
-                        buildWhen: (previous, current) =>
-                            current is ListPaginate,
-                        builder: (context, state) {
-                          if (state is ListPaginateLoading) {
-                            return const Center(
-                              child: CircularProgressIndicator.adaptive(),
-                            );
-                          }
-
-                          return const SizedBox();
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            }
-
-            return const SizedBox();
-          },
+    return PaginationListener(
+      onPaginate: context.read<DropdownCubit>().fetchSuppliersPaginate,
+      child: ListView.separated(
+        itemBuilder: (context, index) => GestureDetector(
+          onTap: () => onTap(suppliers[index]),
+          child: DropdownModalItem(child: Text(suppliers[index].value)),
         ),
+        separatorBuilder: (context, index) => const Gap(12),
+        itemCount: suppliers.length,
+        padding: const .symmetric(vertical: 12),
+        shrinkWrap: true,
       ),
     );
   }
