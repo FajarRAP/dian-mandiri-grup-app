aa631a5fc80581c396aa6b5cb62be3b31aa651ed
Fajar Riansyah <fajary781@gmail.com>
2025-12-03 02:54:08 +0700

    refactor (typedefs): add ListDropdownUseCase typedef for improved type safety in dropdown handling

--- lib/core/utils/typedefs.dart
+++ lib/core/utils/typedefs.dart
@@ -1,8 +1,15 @@
+import 'package:dartz/dartz.dart';
+
+import '../common/dropdown_entity.dart';
+import '../errors/failure.dart';
+
 typedef JsonMap = Map<String, dynamic>;
 
 typedef ListJsonMap = List<Map<String, dynamic>>;
 
 typedef ListJsonMapNullable = List<Map<String, dynamic>?>;
+
+typedef ListDropdownUseCase = Future<Either<Failure, List<DropdownEntity>>>;
 
 extension JsonParser on JsonMap {
   DateTime? toNullableDateTime(String key) {
