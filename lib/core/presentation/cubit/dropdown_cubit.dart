561eb2a6caea68ac8c4a6dfcc0ae7c636e6f9d7f
Fajar Riansyah <fajary781@gmail.com>
2025-12-03 02:55:45 +0700

    refactor (dropdown cubit): streamline fetch methods and enhance parameter handling for suppliers and purchase notes

--- lib/core/presentation/cubit/dropdown_cubit.dart
+++ lib/core/presentation/cubit/dropdown_cubit.dart
@@ -6,6 +6,7 @@ import '../../../features/warehouse/domain/usecases/fetch_purchase_notes_dropdow
 import '../../common/dropdown_entity.dart';
 import '../../errors/failure.dart';
 import '../../usecase/use_case.dart';
+import '../../utils/typedefs.dart';
 
 part 'dropdown_state.dart';
 
@@ -21,7 +22,56 @@ class DropdownCubit extends Cubit<DropdownState> {
   final FetchSuppliersDropdownUseCase _fetchSuppliersDropdownUseCase;
   final FetchPurchaseNotesDropdownUseCase _fetchPurchaseNotesDropdownUseCase;
 
-  Future<void> fetchSuppliers({String query = '', bool showAll = false}) async {
+  Future<void> fetchSuppliers({
+    String query = '',
+    bool showAll = false,
+  }) async => _fetch(
+    usecase: () async {
+      final params = FetchSuppliersDropdownUseCaseParams(
+        search: SearchParams(query: query),
+        showAll: showAll,
+      );
+      return await _fetchSuppliersDropdownUseCase(params);
+    },
+  );
+
+  Future<void> fetchSuppliersPaginate() async => await _paginate(
+    usecase: () async {
+      final params = FetchSuppliersDropdownUseCaseParams(
+        paginate: PaginateParams(page: state.currentPage + 1),
+        search: SearchParams(query: state.query),
+      );
+      return await _fetchSuppliersDropdownUseCase(params);
+    },
+  );
+
+  Future<void> fetchPurchaseNotes({
+    String query = '',
+    bool showAll = false,
+  }) async => await _fetch(
+    usecase: () async {
+      final params = FetchPurchaseNotesDropdownUseCaseParams(
+        search: SearchParams(query: query),
+        showAll: showAll,
+      );
+      return await _fetchPurchaseNotesDropdownUseCase(params);
+    },
+  );
+
+  Future<void> fetchPurchaseNotesPaginate() async => await _paginate(
+    usecase: () async {
+      final params = FetchPurchaseNotesDropdownUseCaseParams(
+        paginate: PaginateParams(page: state.currentPage + 1),
+        search: SearchParams(query: state.query),
+      );
+      return await _fetchPurchaseNotesDropdownUseCase(params);
+    },
+  );
+
+  Future<void> _fetch({
+    String query = '',
+    required ListDropdownUseCase Function() usecase,
+  }) async {
     emit(
       state.copyWith(
         status: .inProgress,
@@ -32,83 +82,22 @@ class DropdownCubit extends Cubit<DropdownState> {
       ),
     );
 
-    final params = FetchSuppliersDropdownUseCaseParams(
-      search: SearchParams(query: query),
-      showAll: showAll,
-    );
-    final result = await _fetchSuppliersDropdownUseCase(params);
+    final result = await usecase();
 
     result.fold(
       (failure) => emit(state.copyWith(status: .failure, failure: failure)),
-      (suppliers) => emit(state.copyWith(status: .success, items: suppliers)),
+      (items) => emit(state.copyWith(status: .success, items: items)),
     );
   }
 
-  Future<void> fetchSuppliersPaginate() async {
+  Future<void> _paginate({
+    required ListDropdownUseCase Function() usecase,
+  }) async {
     if (state.hasReachedMax || state.isPaginating) return;
 
     emit(state.copyWith(isPaginating: true));
 
-    final params = FetchSuppliersDropdownUseCaseParams(
-      paginate: PaginateParams(page: state.currentPage + 1),
-      search: SearchParams(query: state.query),
-    );
-    final result = await _fetchSuppliersDropdownUseCase(params);
-
-    result.fold(
-      (failure) => emit(
-        state.copyWith(
-          status: .failure,
-          hasReachedMax: true,
-          isPaginating: false,
-          failure: failure,
-        ),
-      ),
-      (suppliers) => emit(
-        state.copyWith(
-          status: .success,
-          isPaginating: false,
-          hasReachedMax: suppliers.isEmpty,
-          currentPage: state.currentPage + 1,
-          items: [...state.items, ...suppliers],
-        ),
-      ),
-    );
-  }
-
-  Future<void> fetchPurchaseNotes({String query = '', bool showAll = false}) async {
-    emit(
-      state.copyWith(
-        status: .inProgress,
-        currentPage: 1,
-        hasReachedMax: false,
-        isPaginating: false,
-        query: query,
-      ),
-    );
-
-    final params = FetchPurchaseNotesDropdownUseCaseParams(
-      search: SearchParams(query: query),
-      showAll: showAll,
-    );
-    final result = await _fetchPurchaseNotesDropdownUseCase(params);
-
-    result.fold(
-      (failure) => emit(state.copyWith(status: .failure, failure: failure)),
-      (suppliers) => emit(state.copyWith(status: .success, items: suppliers)),
-    );
-  }
-
-  Future<void> fetchPurchaseNotesPaginate() async {
-    if (state.hasReachedMax || state.isPaginating) return;
-
-    emit(state.copyWith(isPaginating: true));
-
-    final params = FetchPurchaseNotesDropdownUseCaseParams(
-      paginate: PaginateParams(page: state.currentPage + 1),
-      search: SearchParams(query: state.query),
-    );
-    final result = await _fetchPurchaseNotesDropdownUseCase(params);
+    final result = await usecase();
 
     result.fold(
       (failure) => emit(
