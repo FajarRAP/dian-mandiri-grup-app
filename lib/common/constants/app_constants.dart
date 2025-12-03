1e51f972385befbc24cdba8cf0d431959c4a18f5
Fajar Riansyah <fajary781@gmail.com>
2025-12-03 14:16:37 +0700

    feat: add AppConfigs class for environment variable management

--- lib/common/constants/app_constants.dart
+++ lib/common/constants/app_constants.dart
@@ -1,11 +1,6 @@
 class AppConstants {
   const AppConstants._();
 
-  static const apiUrl = String.fromEnvironment('API_URL');
-  static const accessTokenKey = String.fromEnvironment('ACCESS_TOKEN_KEY');
-  static const refreshTokenKey = String.fromEnvironment('REFRESH_TOKEN_KEY');
-  static const userKey = String.fromEnvironment('USER_KEY');
-
   static const scanStage = 'scan';
   static const pickUpStage = 'pick_up';
   static const checkStage = 'check';
