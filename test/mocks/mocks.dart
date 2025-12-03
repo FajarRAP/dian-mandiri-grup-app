36c9cb2cad8c8cfb2613affd6cbb14a898407f10
Fajar Riansyah <fajary781@gmail.com>
2025-12-03 17:01:12 +0700

    refactor (mocks): add mock classes for RequestInterceptorHandler and ErrorInterceptorHandler

--- test/mocks/mocks.dart
+++ test/mocks/mocks.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:ship_tracker/core/network/auth_status_stream.dart';
+import 'package:ship_tracker/core/network/token_refresh_service.dart';
 import 'package:ship_tracker/core/services/file_service.dart';
 import 'package:ship_tracker/core/services/google_sign_in_service.dart';
 import 'package:ship_tracker/features/auth/data/datasources/auth_local_data_source.dart';
@@ -11,6 +13,12 @@ import 'package:ship_tracker/features/tracker/data/datasources/shipment_remote_d
 import 'package:ship_tracker/features/warehouse/data/datasources/warehouse_remote_data_source.dart';
 
 class MockDio extends Mock implements Dio {}
+
+class MockRequestInterceptorHandler extends Mock
+    implements RequestInterceptorHandler {}
+
+class MockErrorInterceptorHandler extends Mock
+    implements ErrorInterceptorHandler {}
 
 class MockStorage extends Mock implements FlutterSecureStorage {}
 
@@ -32,3 +40,7 @@ class MockSupplierRemoteDataSource extends Mock
 
 class MockWarehouseRemoteDataSource extends Mock
     implements WarehouseRemoteDataSource {}
+
+class MockTokenRefreshService extends Mock implements TokenRefreshService {}
+
+class MockAuthStatusStream extends Mock implements AuthStatusStream {}
