dc740806fcf82cf99cdd1933edbb8b670692adf6
Fajar Riansyah <fajary781@gmail.com>
2025-12-03 14:34:44 +0700

    refactor (auth): streamline AuthCubit and AuthState by removing unused use cases and states

--- lib/features/auth/presentation/cubit/auth_cubit.dart
+++ lib/features/auth/presentation/cubit/auth_cubit.dart
@@ -4,99 +4,35 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
+import '../../../../core/errors/failure.dart';
 import '../../../../core/network/auth_status_stream.dart';
 import '../../../../core/usecase/use_case.dart';
-import '../../domain/entities/user_entity.dart';
-import '../../domain/usecases/fetch_user_from_storage_use_case.dart';
-import '../../domain/usecases/fetch_user_use_case.dart';
-import '../../domain/usecases/refresh_token_use_case.dart';
-import '../../domain/usecases/sign_in_use_case.dart';
 import '../../domain/usecases/sign_out_use_case.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit({
-    required FetchUserUseCase fetchUserUseCase,
-    required FetchUserFromStorageUseCase fetchUserFromStorageUseCase,
-    required RefreshTokenUseCase refreshTokenUseCase,
-    required SignInUseCase signInUseCase,
     required SignOutUseCase signOutUseCase,
     required AuthStatusStream authStatusStream,
-  }) : _fetchUserUseCase = fetchUserUseCase,
-       _fetchUserFromStorageUseCase = fetchUserFromStorageUseCase,
-       _refreshTokenUseCase = refreshTokenUseCase,
-       _signInUseCase = signInUseCase,
-       _signOutUseCase = signOutUseCase,
+  }) : _signOutUseCase = signOutUseCase,
        _authStatusStream = authStatusStream,
        super(AuthInitial()) {
     _listenAuthStatus();
   }
 
-  final FetchUserUseCase _fetchUserUseCase;
-  final FetchUserFromStorageUseCase _fetchUserFromStorageUseCase;
-  final RefreshTokenUseCase _refreshTokenUseCase;
-  final SignInUseCase _signInUseCase;
   final SignOutUseCase _signOutUseCase;
   final AuthStatusStream _authStatusStream;
   late StreamSubscription<bool> _authStatusSubscription;
 
-  Future<void> fetchUser() async {
-    emit(FetchUserLoading());
-
-    final result = await _fetchUserUseCase();
-
-    result.fold(
-      (failure) => emit(FetchUserError(message: failure.message)),
-      (user) => emit(FetchUserLoaded(user: user)),
-    );
-  }
-
-  Future<void> fetchUserFromStorage() async {
-    emit(FetchUserLoading());
-
-    final userFromStorage = await _fetchUserFromStorageUseCase();
-
-    userFromStorage.fold(
-      (failure) => emit(FetchUserError(message: failure.message)),
-      (user) => user == null
-          ? emit(FetchUserError(message: 'No user found in storage'))
-          : emit(FetchUserLoaded(user: user)),
-    );
-  }
-
-  Future<void> refreshToken({required String? refreshToken}) async {
-    emit(RefreshTokenLoading());
-
-    final params = RefreshTokenUseCaseParams(refreshToken: '$refreshToken');
-    final result = await _refreshTokenUseCase(params);
-
-    result.fold(
-      (failure) => emit(RefreshTokenError(message: failure.message)),
-      (message) => emit(RefreshTokenLoaded()),
-    );
-  }
-
-  Future<void> signIn() async {
-    emit(const SignInLoading());
-
-    final result = await _signInUseCase();
-
-    result.fold(
-      (failure) => emit(SignInError(message: failure.message)),
-      (signIn) =>
-          emit(SignInLoaded(message: signIn.message, user: signIn.user)),
-    );
-  }
-
   Future<void> signOut() async {
-    emit(SignOutLoading());
+    emit(const AuthLoading());
 
     final result = await _signOutUseCase();
 
     result.fold(
-      (failure) => emit(SignOutError(message: failure.message)),
-      (message) => emit(SignOutLoaded(message: message)),
+      (failure) => emit(AuthFailure(failure: failure)),
+      (message) => emit(Unauthenticated(message: message)),
     );
   }
 
@@ -105,7 +41,7 @@ class AuthCubit extends Cubit<AuthState> {
       isAuthenticated,
     ) {
       if (!isAuthenticated) {
-        emit(const Unauthenticated(message: 'Session Ended'));
+        signOut();
       }
     });
   }
--- lib/features/auth/presentation/cubit/auth_state.dart
+++ lib/features/auth/presentation/cubit/auth_state.dart
@@ -10,94 +10,24 @@ sealed class AuthState extends Equatable {
 
 final class AuthInitial extends AuthState {}
 
-class FetchUser extends AuthState {}
-
-class RefreshToken extends AuthState {}
-
-class UpdateProfile extends AuthState {}
-
-class FetchUserLoading extends FetchUser {}
-
-class SignIn extends AuthState {
-  const SignIn();
+class AuthLoading extends AuthState {
+  const AuthLoading();
 }
-
-class SignOut extends AuthState {}
-
-class FetchUserLoaded extends FetchUser {
-  FetchUserLoaded({required this.user});
-
-  final UserEntity user;
-}
-
-class FetchUserError extends FetchUser {
-  FetchUserError({required this.message});
-
-  final String message;
-}
-
-class RefreshTokenLoading extends RefreshToken {}
-
-class RefreshTokenLoaded extends RefreshToken {}
-
-class RefreshTokenError extends RefreshToken {
-  RefreshTokenError({required this.message});
-
-  final String message;
-}
-
-class SignInLoading extends SignIn {
-  const SignInLoading();
-}
-
-class SignInLoaded extends SignIn {
-  const SignInLoaded({required this.message, required this.user});
-
-  final String message;
-  final UserEntity user;
-
-  @override
-  List<Object?> get props => [message, user];
-}
-
-class SignInError extends SignIn {
-  const SignInError({required this.message});
-
-  final String message;
-}
-
-class SignOutLoading extends SignOut {}
-
-class SignOutLoaded extends SignOut {
-  SignOutLoaded({required this.message});
-
-  final String message;
-}
-
-class SignOutError extends SignOut {
-  SignOutError({required this.message});
-
-  final String message;
-}
-
-class UpdateProfileLoading extends UpdateProfile {}
-
-class UpdateProfileLoaded extends UpdateProfile {
-  UpdateProfileLoaded({required this.message});
-
-  final String message;
-}
-
-class UpdateProfileError extends UpdateProfile {
-  UpdateProfileError({required this.message});
-
-  final String message;
-}
-
-class RefreshTokenExpired extends AuthState {}
 
 class Unauthenticated extends AuthState {
   const Unauthenticated({required this.message});
 
   final String message;
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class AuthFailure extends AuthState {
+  const AuthFailure({required this.failure});
+
+  final Failure failure;
+
+  @override
+  List<Object?> get props => [failure];
 }
